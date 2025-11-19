64acd7658dc9d599de8b4cb07e4dd7d52a87cda6
EvannBloutin <evann.bloutin@epitech.eu>
2025-11-19 18:31:18 +0100

    feat: add detailed documentation and method declarations to SFMLRenderer class

--- client/src/graphics/SFML/SFMLRenderer.cpp
+++ client/src/graphics/SFML/SFMLRenderer.cpp
@@ -1,3 +1,32 @@
+/*
+** EPITECH PROJECT, 2025
+** real_r_type
+** File description:
+** SFMLRenderer
+*/
+
 #include "SFMLRenderer.hpp"
 
-// SFMLRenderer implementation
+void SFMLRenderer::createWindow(int width, int height, const std::string& title) {
+    window.create(sf::VideoMode(width, height), title);
+}
+
+bool SFMLRenderer::isOpen() const {
+    return window.isOpen();
+}
+
+void SFMLRenderer::close() {
+    window.close();
+}
+
+void SFMLRenderer::clear() {
+    window.clear();
+}
+
+void SFMLRenderer::display() {
+    window.display();
+}
+
+bool SFMLRenderer::pollEvent(sf::Event& event) {
+    return window.pollEvent(event);
+}

a5924cb5c89f4b6773aed2357e6b42cacf02834d
romain1717 <romain.berard@epitech.eu>
2025-11-21 15:32:09 +0100

    fix: improve exception handling in WinHandler methods for robustness

--- server/src/signal/WinHandler/WinHandler.cpp
+++ server/src/signal/WinHandler/WinHandler.cpp
@@ -32,33 +32,47 @@ WinHandler::WinHandler() : running(false)
 
 WinHandler::~WinHandler()
 {
-    stop();
-    if (instance == this)
-        instance = nullptr;
+    try {
+        stop();
+    } catch (...) {
+        std::cerr << "{WinHandler::~WinHandler} Exception occurred during destruction" << std::endl;
+    }
+    instance = nullptr;
 }
 
 void WinHandler::start() noexcept
 {
-    running = true;
+    try {
+        if (running.load())
+            return;
+        running = true;
 
-    std::signal(SIGINT, &WinHandler::handleSignal);
-    std::signal(SIGTERM, &WinHandler::handleSignal);
+        std::signal(SIGINT, &WinHandler::handleSignal);
+        std::signal(SIGTERM, &WinHandler::handleSignal);
+    } catch (...) {
+        std::cerr << "{WinHandler::start} Exception occurred while starting signal handler" << std::endl;
+    }
 }
 
 void WinHandler::stop() noexcept
 {
-    if (!running.load())
-        return;
+    try {
+        running = false;
 
-    running = false;
-
-    std::signal(SIGINT, SIG_DFL);
-    std::signal(SIGTERM, SIG_DFL);
+        std::signal(SIGINT, SIG_DFL);
+        std::signal(SIGTERM, SIG_DFL);
+    } catch (...) {
+        std::cerr << "{WinHandler::stop} Exception occurred while stopping signal handler" << std::endl;
+    }
 }
 
 void WinHandler::registerCallback(SignalType type, std::function<void()> callback) noexcept
 {
-    callbacks[type] = std::move(callback);
+    try {
+        callbacks[type] = std::move(callback);
+    } catch (...) {
+        std::cerr << "{WinHandler::registerCallback} Exception occurred while registering callback" << std::endl;
+    }
 }
 
 void WinHandler::handleSignal(int signum)

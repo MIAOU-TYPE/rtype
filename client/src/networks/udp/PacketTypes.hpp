b4e43e3789d6665e6f87b17800c3718c394a7379
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-11-25 00:09:10 +0100

    refactor: Update PacketTypes.hpp to improve header structure and maintainability

--- client/src/networks/udp/PacketTypes.hpp
+++ client/src/networks/udp/PacketTypes.hpp
@@ -1,6 +1,36 @@
-#ifndef PACKET_TYPES_HPP
-#define PACKET_TYPES_HPP
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** PacketTypes
+*/
 
-// PacketTypes header
+#pragma once
 
-#endif // PACKET_TYPES_HPP
+#include <cstdint>
+
+namespace Network
+{
+
+    /**
+     * @brief Enumeration of all network packet types
+     *
+     * Defines the different types of packets that can be exchanged
+     * between client and server in the R-Type game protocol.
+     */
+    enum class PacketType : uint8_t {
+        CONNECT,        ///< Client -> Server : Request to connect
+        INPUT,          ///< Client -> Server : Send player input
+        PING,           ///< Client -> Server : Ping request
+        DISCONNECT,     ///< Client -> Server : Notify disconnection
+        ACCEPT,         ///< Server -> Client : Accept connection
+        REJECT,         ///< Server -> Client : Reject connection
+        SNAPSHOT,       ///< Server -> Client : Send game state
+        ENTITY_CREATE,  ///< Server -> Client : Notify entity creation
+        ENTITY_DESTROY, ///< Server -> Client : Notify entity destruction
+        PONG,           ///< Server -> Client : Pong response
+        DAMAGE_EVENT,   ///< Server -> Client : Notify damage event
+        GAME_OVER,      ///< Server -> Client : Notify game over
+    };
+
+} // namespace Network

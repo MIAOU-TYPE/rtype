4c02fb407887512684d124b1220024d0e3a77280
EvannBloutin <evann.bloutin@epitech.eu>
2025-11-24 21:58:04 +0100

    feat: enhance SFMLTextureManager with texture loading and sprite creation functionality

--- client/src/graphics/SFML/SFMLTextureManager.cpp
+++ client/src/graphics/SFML/SFMLTextureManager.cpp
@@ -1,3 +1,87 @@
-#include "SFMLTextureManager.hpp"
+/*
+** EPITECH PROJECT, 2025
+** real_r_type
+** File description:
+** SFMLTextureManager
+*/
 
-// SFMLTextureManager implementation
+#include "SFMLTextureManager.hpp"
+#include "SFMLRenderer.hpp"
+
+using namespace Graphics;
+
+SFMLSprite::SFMLSprite(sf::Sprite sprite) : _sprite(std::move(sprite))
+{
+}
+
+void SFMLSprite::setPosition(float x, float y)
+{
+    _sprite.setPosition(x, y);
+}
+
+float SFMLSprite::getWidth() const
+{
+    return _sprite.getGlobalBounds().width;
+}
+
+float SFMLSprite::getHeight() const
+{
+    return _sprite.getGlobalBounds().height;
+}
+
+void SFMLSprite::setScale(float scaleX, float scaleY)
+{
+    _sprite.setScale(scaleX, scaleY);
+}
+
+const sf::Sprite &SFMLSprite::getSFMLSprite() const
+{
+    return _sprite;
+}
+
+bool SFMLTextureManager::loadTexture(const std::string &filePath)
+{
+    if (_textures.find(filePath) != _textures.end()) {
+        return true;
+    }
+
+    sf::Texture texture;
+    if (!texture.loadFromFile(filePath)) {
+        return false;
+    }
+
+    _textures[filePath] = std::move(texture);
+    return true;
+}
+
+std::unique_ptr<ISprite> SFMLTextureManager::createSprite(const std::string &texturePath)
+{
+    if (!loadTexture(texturePath)) {
+        return nullptr;
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(_textures[texturePath]);
+
+    return std::make_unique<SFMLSprite>(std::move(sprite));
+}
+
+void SFMLTextureManager::renderSprite(const ISprite &sprite)
+{
+    if (!_renderer) {
+        return;
+    }
+
+    const auto *sfmlSprite = dynamic_cast<const SFMLSprite *>(&sprite);
+    if (sfmlSprite) {
+        auto *sfmlRenderer = dynamic_cast<SFMLRenderer *>(_renderer.get());
+        if (sfmlRenderer) {
+            sfmlRenderer->drawSprite(sfmlSprite->getSFMLSprite());
+        }
+    }
+}
+
+void SFMLTextureManager::setRenderer(std::shared_ptr<IRenderer> renderer)
+{
+    _renderer = renderer;
+}
--- client/src/graphics/SFML/SFMLTextureManager.hpp
+++ client/src/graphics/SFML/SFMLTextureManager.hpp
@@ -1,6 +1,109 @@
-#ifndef SFML_TEXTURE_MANAGER_HPP
-#define SFML_TEXTURE_MANAGER_HPP
+/*
+** EPITECH PROJECT, 2025
+** real_r_type
+** File description:
+** SFMLTextureManager
+*/
 
-// SFMLTextureManager header
+#pragma once
 
-#endif // SFML_TEXTURE_MANAGER_HPP
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include "IRenderer.hpp"
+#include "ITextureManager.hpp"
+#include <unordered_map>
+
+/**
+ * @namespace Graphics
+ * @brief Contains all graphics-related classes and interfaces.
+ */
+namespace Graphics
+{
+    /**
+     * @class SFMLSprite
+     * @brief SFML implementation of the ISprite interface.
+     */
+    class SFMLSprite : public ISprite {
+      public:
+        /**
+         * @brief Constructor for SFMLSprite.
+         * @param sprite The SFML sprite to wrap.
+         */
+        explicit SFMLSprite(sf::Sprite sprite);
+
+        /**
+         * @brief Sets the position of the sprite.
+         * @param x The X coordinate.
+         * @param y The Y coordinate.
+         */
+        void setPosition(float x, float y) override;
+
+        /**
+         * @brief Gets the width of the sprite.
+         * @return The width of the sprite.
+         */
+        float getWidth() const override;
+
+        /**
+         * @brief Gets the height of the sprite.
+         * @return The height of the sprite.
+         */
+        float getHeight() const override;
+
+        /**
+         * @brief Sets the scale of the sprite.
+         * @param scaleX The X scale factor.
+         * @param scaleY The Y scale factor.
+         */
+        void setScale(float scaleX, float scaleY) override;
+
+        /**
+         * @brief Gets the underlying SFML sprite.
+         * @return Reference to the SFML sprite.
+         */
+        const sf::Sprite &getSFMLSprite() const;
+
+      private:
+        sf::Sprite _sprite; ///< The underlying SFML sprite
+    };
+
+    /**
+     * @class SFMLTextureManager
+     * @brief SFML implementation of the ITextureManager interface.
+     *
+     * This class provides texture management functionality using the SFML library.
+     */
+    class SFMLTextureManager : public ITextureManager {
+      public:
+        /**
+         * @brief Loads a texture from file.
+         * @param filePath The path to the texture file.
+         * @return True if the texture was loaded successfully, false otherwise.
+         */
+        bool loadTexture(const std::string &filePath) override;
+
+        /**
+         * @brief Creates a sprite from a loaded texture.
+         * @param texturePath The path to the texture file.
+         * @return A unique pointer to the created sprite.
+         */
+        std::unique_ptr<ISprite> createSprite(const std::string &texturePath) override;
+
+        /**
+         * @brief Renders a sprite to the screen.
+         * @param sprite The sprite to render.
+         */
+        void renderSprite(const ISprite &sprite) override;
+
+        /**
+         * @brief Sets the renderer for rendering sprites.
+         * @param renderer Shared pointer to the renderer.
+         */
+        void setRenderer(std::shared_ptr<IRenderer> renderer) override;
+
+      private:
+        std::unordered_map<std::string, sf::Texture> _textures; ///< Map of loaded textures
+        std::shared_ptr<IRenderer> _renderer;                   ///< The renderer interface
+    };
+
+} // namespace Graphics

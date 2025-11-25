4fb87b29379b22b9c79c7d318497af6ee9c61ad9
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-11-25 14:21:37 +0100

    refactor: Rearrange include directives for improved cross-platform compatibility

--- client/src/networks/udp/SocketConfig.hpp
+++ client/src/networks/udp/SocketConfig.hpp
@@ -7,9 +7,15 @@
 
 #pragma once
 #include <cstdint>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+#else
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <sys/types.h>
+#endif
 
 /**
  * @namespace Network

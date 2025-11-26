f8e3a9b98c09b415929affbf4255990c5d8f1f27
EvannBloutin <evann.bloutin@epitech.eu>
2025-11-26 15:39:55 +0100

    feat: implement SFMLEntityDrawing class with entity creation and rendering methods

--- client/src/graphics/SFML/SFMLEntityDrawing.cpp
+++ client/src/graphics/SFML/SFMLEntityDrawing.cpp
@@ -1,3 +1,62 @@
-#include "SFMLEntityDrawing.hpp"
+/*
+** EPITECH PROJECT, 2025
+** real_r_type
+** File description:
+** SFMLEntityDrawing
+*/
 
-// SFMLEntityDrawing implementation
+#include "SFMLEntityDrawing.hpp"
+#include <vector>
+
+using namespace Graphics;
+
+std::unordered_map<std::string, SFMLEntityDrawing::SpriteInfo> SFMLEntityDrawing::_spriteInfo = {
+    {"player", {"client/assets/sprites/player.png", 33.1f, 18.0f}},
+    {"enemie", {"client/assets/sprites/enemie.png", 65.1f, 66.0f}}};
+
+SFMLEntityDrawing::SFMLEntityDrawing(
+    std::shared_ptr<IRenderer> renderer, std::shared_ptr<ITextureManager> textureManager)
+    : _renderer(renderer), _textureManager(textureManager)
+{
+    if (!_renderer) {
+        throw std::runtime_error("Renderer cannot be null");
+    }
+    if (!_textureManager) {
+        throw std::runtime_error("Texture manager cannot be null");
+    }
+}
+
+std::shared_ptr<GraphicalEntity> SFMLEntityDrawing::createEntity(float x, float y, const std::string &spriteName)
+{
+    try {
+        auto entity = std::make_shared<GraphicalEntity>(x, y, spriteName, _textureManager);
+        _entities.push_back(entity);
+        return entity;
+    } catch (const std::exception &e) {
+        throw std::runtime_error("Failed to create entity '" + spriteName + "': " + std::string(e.what()));
+    }
+}
+
+void SFMLEntityDrawing::renderAllEntities()
+{
+    for (auto &entity : _entities) {
+        if (entity) {
+            entity->render(_renderer);
+        }
+    }
+}
+
+SFMLEntityDrawing::SpriteInfo SFMLEntityDrawing::getSpriteInfoFromName(const std::string &spriteName)
+{
+    auto it = _spriteInfo.find(spriteName);
+    if (it != _spriteInfo.end()) {
+        return it->second;
+    }
+
+    return {"client/assets/sprites/" + spriteName + ".png", 32.0f, 32.0f};
+}
+
+std::string SFMLEntityDrawing::getSpritePathFromName(const std::string &spriteName)
+{
+    return getSpriteInfoFromName(spriteName).path;
+}
--- client/src/graphics/SFML/SFMLEntityDrawing.hpp
+++ client/src/graphics/SFML/SFMLEntityDrawing.hpp
@@ -1,6 +1,83 @@
-#ifndef SFML_ENTITY_DRAWING_HPP
-#define SFML_ENTITY_DRAWING_HPP
+/*
+** EPITECH PROJECT, 2025
+** real_r_type
+** File description:
+** SFMLEntityDrawing
+*/
 
-// SFMLEntityDrawing header
+#pragma once
 
-#endif // SFML_ENTITY_DRAWING_HPP
+#include <memory>
+#include <string>
+#include <vector>
+#include "IRenderer.hpp"
+#include "ITextureManager.hpp"
+#include "SFMLEntityCreation.hpp"
+#include <unordered_map>
+
+/**
+ * @namespace Graphics
+ * @brief Contains all graphics-related classes and interfaces.
+ */
+namespace Graphics
+{
+    /**
+     * @class SFMLEntityDrawing
+     * @brief Manager for drawing entities using SFML.
+     *
+     * This class manages the creation and rendering of graphical entities.
+     */
+    class SFMLEntityDrawing {
+      public:
+        /**
+         * @brief Constructor for SFMLEntityDrawing.
+         * @param renderer Shared pointer to the renderer.
+         * @param textureManager Shared pointer to the texture manager.
+         */
+        SFMLEntityDrawing(std::shared_ptr<IRenderer> renderer, std::shared_ptr<ITextureManager> textureManager);
+
+        /**
+         * @brief Creates and adds a new entity.
+         * @param x The X position of the entity.
+         * @param y The Y position of the entity.
+         * @param spriteName The name of the sprite.
+         * @return Shared pointer to the created entity.
+         */
+        std::shared_ptr<GraphicalEntity> createEntity(float x, float y, const std::string &spriteName);
+
+        /**
+         * @brief Renders all entities.
+         */
+        void renderAllEntities();
+
+        /**
+         * @brief Structure containing sprite information.
+         */
+        struct SpriteInfo {
+            std::string path; ///< Path to the sprite file
+            float width;      ///< Width of the sprite
+            float height;     ///< Height of the sprite
+        };
+
+        /**
+         * @brief Gets the sprite info from sprite name.
+         * @param spriteName The name of the sprite.
+         * @return The sprite information (path and dimensions).
+         */
+        static SpriteInfo getSpriteInfoFromName(const std::string &spriteName);
+
+        /**
+         * @brief Gets the sprite path from sprite name.
+         * @param spriteName The name of the sprite.
+         * @return The full path to the sprite file.
+         */
+        static std::string getSpritePathFromName(const std::string &spriteName);
+
+      private:
+        std::shared_ptr<IRenderer> _renderer;                           ///< The renderer
+        std::shared_ptr<ITextureManager> _textureManager;               ///< The texture manager
+        std::vector<std::shared_ptr<GraphicalEntity>> _entities;        ///< List of entities
+        static std::unordered_map<std::string, SpriteInfo> _spriteInfo; ///< Mapping sprite names to info
+    };
+
+} // namespace Graphics

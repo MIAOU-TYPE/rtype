cc532c4e323fb7bfdd8fc82edd649bd4d14db11f
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-11-26 14:23:02 +0100

    feat: Add UdpClientPacket class for UDP packet handling

---
+++ client/src/networks/udp/UdpClientPacket.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** UdpClientPacket
+*/
+
+#include "UdpClientPacket.hpp"
+
+namespace Network
+{
+    UdpClientPacket::UdpClientPacket() : AClientPacket()
+    {
+        // Base class constructor handles initialization
+    }
+} // namespace Network

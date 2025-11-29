d4d6fcb31bcb65f55481b8a017155297f51fec4c
romain1717 <romain.berard@epitech.eu>
2025-11-29 17:40:31 +0100

    refactor: extract signal handler initialization into a separate function

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -10,18 +10,26 @@
 #include "SignalHandler.hpp"
 #include "UDPServer.hpp"
 
+
+static std::shared_ptr<Signal::SignalHandler> startSignalHandler(std::shared_ptr<Server::IServer> server)
+{
+    std::shared_ptr<Signal::SignalHandler> signalHandler = std::make_shared<Signal::SignalHandler>();
+
+    signalHandler->start();
+    signalHandler->registerCallback(Signal::SignalType::Interrupt, [&server]() {
+        if (server->isRunning())
+            server->setRunning(false);
+    });
+    return signalHandler;
+}
+
 int main(void)
 {
     std::string ip = "127.0.0.1";
     uint16_t port = 8080;
 
     std::shared_ptr<Server::IServer> server = std::make_shared<Server::UDPServer>();
-    Signal::SignalHandler signalHandler;
-    signalHandler.start();
-    signalHandler.registerCallback(Signal::SignalType::Interrupt, [&server]() {
-        if (server->isRunning())
-            server->setRunning(false);
-    });
+    auto signalHandler =  startSignalHandler(server);
     try {
         server->configure(ip, port);
         server->start();
@@ -30,9 +38,9 @@ int main(void)
         server->stop();
     } catch (const Server::ServerError &e) {
         std::cerr << "{Main}" << e.what() << std::endl;
-        signalHandler.stop();
+        signalHandler->stop();
         return 1;
     }
-    signalHandler.stop();
+    signalHandler->stop();
     return 0;
 }

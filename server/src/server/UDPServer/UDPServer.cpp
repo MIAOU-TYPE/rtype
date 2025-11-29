da8dbea63d84669433e72ca7dc3deefb5617e620
romain1717 <romain.berard@epitech.eu>
2025-11-29 20:57:59 +0100

    feat: refactor UDPServer to use isRunning and setRunning methods for state management

--- server/src/server/UDPServer/UDPServer.cpp
+++ server/src/server/UDPServer/UDPServer.cpp
@@ -11,6 +11,7 @@ using namespace Server;
 
 UDPServer::UDPServer() : AServer(), _rxBuffer(1024)
 {
+    setRunning(false);
 #ifdef _WIN32
     WSADATA wsa;
     const int r = WSAStartup(MAKEWORD(2, 2), &wsa);
@@ -21,7 +22,7 @@ UDPServer::UDPServer() : AServer(), _rxBuffer(1024)
 
 UDPServer::~UDPServer()
 {
-    if (_isRunning) {
+    if (isRunning()) {
         stop();
     }
 #ifdef _WIN32
@@ -31,7 +32,7 @@ UDPServer::~UDPServer()
 
 void UDPServer::start()
 {
-    if (_isRunning || _socketFd != kInvalidSocket)
+    if (isRunning() || _socketFd != kInvalidSocket)
         throw ServerError("{UDPServer::start} Server is already running");
 
     try {
@@ -46,13 +47,13 @@ void UDPServer::start()
         _socketFd = kInvalidSocket;
         throw ServerError(std::string("{UDPServer::start}") + e.what());
     }
-    _isRunning = true;
+    setRunning(true);
     std::cout << "{UDPServer::start} UDP Server started on " << _ip << ":" << _port << std::endl;
 }
 
 void UDPServer::stop()
 {
-    _isRunning = false;
+    setRunning(false);
     Net::NetWrapper::closeSocket(_socketFd);
     _socketFd = kInvalidSocket;
     std::cout << "{UDPServer::stop} UDP Server stopped." << std::endl;
@@ -60,7 +61,9 @@ void UDPServer::stop()
 
 void UDPServer::readPackets()
 {
-    std::shared_ptr<Net::IServerPacket> pkt = std::make_shared<Net::UDPPacket>();
+    if (!isRunning() || _socketFd == kInvalidSocket)
+        return;
+    auto pkt = std::make_shared<Net::UDPPacket>();
     socklen_t addrLen = sizeof(sockaddr_in);
 
     recvfrom_return_t received = Net::NetWrapper::recvFrom(_socketFd, pkt->buffer(), Net::UDPPacket::MAX_SIZE, 0,

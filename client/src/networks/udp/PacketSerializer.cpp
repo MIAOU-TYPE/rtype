117835019b11bf9f286a68b799b39c44c20672ea
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-11-30 22:05:17 +0100

    style: format code for consistency in PacketSerializer

--- client/src/networks/udp/PacketSerializer.cpp
+++ client/src/networks/udp/PacketSerializer.cpp
@@ -14,15 +14,15 @@ namespace Network
         if (packet.entities == nullptr) {
             throw SerializerError("Cannot serialize: PacketSnapshot entities pointer is null");
         }
-        
-        size_t totalSize = sizeof(PacketSnapshot) + entityCount * sizeof(SnapshotEntity);        
+
+        size_t totalSize = sizeof(PacketSnapshot) + entityCount * sizeof(SnapshotEntity);
         std::vector<uint8_t> buffer(totalSize);
         std::memcpy(buffer.data(), &packet, sizeof(PacketSnapshot));
-        
+
         if (entityCount > 0) {
             std::memcpy(buffer.data() + sizeof(PacketSnapshot), packet.entities, entityCount * sizeof(SnapshotEntity));
         }
-        
+
         return buffer;
     }
 } // namespace Network

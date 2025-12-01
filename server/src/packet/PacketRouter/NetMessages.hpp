b77284b6b5ca64ca7642e3d0fa4f7917bf94a642
romain1717 <romain.berard@epitech.eu>
2025-12-01 21:20:59 +0100

    feat: add IMessageSink interface and PlayerInputMessage struct for network message handling

---
+++ server/src/packet/PacketRouter/NetMessages.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** RType
+** File description:
+** ${HEADER_FILENAME}
+*/
+
+#pragma once
+
+struct PlayerInputMessage {
+    bool up = false;
+    bool down = false;
+    bool left = false;
+    bool right = false;
+    bool shoot = false;
+};

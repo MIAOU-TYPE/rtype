8cfc1a237298e772004525be67560e39ff5bb22f
romain1717 <romain.berard@epitech.eu>
2025-12-06 18:51:28 +0100

    refactor: change libraries from SHARED to STATIC and update output directories in CMakeLists

--- shared/NetWrapper/Library/src/NetPlugin.cpp
+++ shared/NetWrapper/Library/src/NetPlugin.cpp
@@ -5,34 +5,40 @@
 ** NetPlugin
 */
 
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+
 #include "NetPlugin.hpp"
 
 extern "C"
 {
-    socketHandle net_socket(int domain, int type, int protocol)
+    EXPORT socketHandle net_socket(int domain, int type, int protocol)
     {
         return ::socket(domain, type, protocol);
     }
 
 #ifdef _WIN32
-    void net_close(socketHandle s)
+    EXPORT void net_close(socketHandle s)
     {
         if (s != kInvalidSocket)
             closesocket(s);
     }
 
-    int net_setOpt(socketHandle s, int level, int optName, const void *optVal, int optLen)
+    EXPORT int net_setOpt(socketHandle s, int level, int optName, const void *optVal, int optLen)
     {
         return ::setsockopt(s, level, optName, (const char *) optVal, optLen);
     }
 
-    recvfrom_return_t net_recvFrom(
+    EXPORT recvfrom_return_t net_recvFrom(
         socketHandle sockFd, void *buf, size_t len, int flags, struct sockaddr *srcAddr, socklen_t *addrLen)
     {
         return ::recvfrom(sockFd, (char *) buf, static_cast<int>(len), flags, srcAddr, (int *) addrLen);
     }
 
-    sendto_return_t net_sendTo(
+    EXPORT sendto_return_t net_sendTo(
         socketHandle sockFd, const void *buf, size_t len, int flags, const struct sockaddr *destAddr, socklen_t addrLen)
     {
         return ::sendto(sockFd, (const char *) buf, static_cast<int>(len), flags, destAddr, static_cast<int>(addrLen));

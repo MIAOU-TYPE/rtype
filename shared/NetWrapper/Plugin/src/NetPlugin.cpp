40c8dc16c236d6cd755e413ae1039699e8d96e85
romain.1717 <romain.berard@epitech.eu>
2025-12-08 00:34:34 +0100

    feat: add net_initNetwork function for Windows network initialization

--- shared/NetWrapper/Plugin/src/NetPlugin.cpp
+++ shared/NetWrapper/Plugin/src/NetPlugin.cpp
@@ -15,6 +15,16 @@
 
 extern "C"
 {
+    EXPORT int net_initNetwork()
+    {
+        #ifdef _WIN32
+            WSADATA wsaData;
+            return WSAStartup(MAKEWORD(2, 2), &wsaData);
+        #else
+            return 0;
+        #endif
+    }
+
     EXPORT socketHandle net_socket(int domain, int type, int protocol)
     {
         return ::socket(domain, type, protocol);

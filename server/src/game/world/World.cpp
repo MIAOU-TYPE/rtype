fc58e3eb582e98ae6c52be4702c80d04d88ac5db
ann7415 <anna.poghosyan@epitech.eu>
2025-12-07 21:21:40 +0100

    feat: implement World class for ECS management and player entity creation

---
+++ server/src/game/world/World.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** World
+*/
+
+#include "World.hpp"
+
+namespace Game
+{
+    World::World()
+    {
+        _registry.registerComponent<Ecs::Position>();
+        _registry.registerComponent<Ecs::Velocity>();
+        _registry.registerComponent<Ecs::Health>();
+        _registry.registerComponent<InputComponent>();
+    }
+
+    Ecs::Entity World::createPlayer()
+    {
+        const Ecs::Entity ent = _registry.createEntity();
+
+        _registry.emplaceComponent<Ecs::Position>(ent, 100.f, 100.f);
+        _registry.emplaceComponent<Ecs::Velocity>(ent, 0.f, 0.f);
+        _registry.emplaceComponent<Ecs::Health>(ent, 100, 100);
+        _registry.emplaceComponent<InputComponent>(ent);
+        return ent;
+    }
+
+    void World::destroyEntity(const Ecs::Entity ent)
+    {
+        _registry.destroyEntity(ent);
+    }
+} // namespace Game

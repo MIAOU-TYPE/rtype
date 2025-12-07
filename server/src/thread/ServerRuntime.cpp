ed7248fc3cd4c58b8627a840ecfd4032def14396
ann7415 <anna.poghosyan@epitech.eu>
2025-12-07 22:00:06 +0100

    feat: refactor ServerRuntime to improve GameServer integration and method signatures

--- server/src/thread/ServerRuntime.cpp
+++ server/src/thread/ServerRuntime.cpp
@@ -11,7 +11,7 @@ using namespace Net::Thread;
 
 ServerRuntime::ServerRuntime(const std::shared_ptr<Server::IServer> &server) : _server(server)
 {
-    _sessionManager = std::make_shared<SessionManager>();
+    _sessionManager = std::make_shared<Server::SessionManager>();
     _gameServer = std::make_shared<Game::GameServer>(_sessionManager, _server);
     _packetRouter = std::make_shared<PacketRouter>(_sessionManager, _gameServer);
 }
@@ -66,29 +66,13 @@ void ServerRuntime::runProcessor() const
 {
     auto last = std::chrono::steady_clock::now();
     while (_server->isRunning()) {
-<<<<<<< HEAD
-        std::shared_ptr<Net::IPacket> packet = nullptr;
-        if (_server->popPacket(packet)) {
-            _packetRouter->handlePacket(packet);
-            // TODO:
-            //  When the real GameServer is implemented (instead of tempMessageSink),
-            //  call gameServer->update(dt) here.
-            //  This will:
-            //  - process inputs stored in ECS components
-            //  - tick movement / physics / collision / AI systems
-            //  - compute world state changes
-            //  - generate a snapshot for clients using PacketFactory
-            //
-            //  This is the place where the game simulation runs every frame.
-=======
-        if (std::shared_ptr<Net::IServerPacket> pkt = nullptr; _server->popPacket(pkt)) {
+        if (std::shared_ptr<IPacket> pkt = nullptr; _server->popPacket(pkt)) {
             _packetRouter->handlePacket(pkt);
 
             auto now = std::chrono::steady_clock::now();
             const float dt = std::chrono::duration<float>(now - last).count();
             last = now;
             _gameServer->update(dt);
->>>>>>> 91f24ae (refactor: update ServerRuntime to include GameServer and improve method signatures)
         }
     }
 }

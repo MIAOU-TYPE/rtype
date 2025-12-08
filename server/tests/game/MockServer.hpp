3a0810a351df33f43e4f9ef8d2d4fb72be083501
romain1717 <romain.berard@epitech.eu>
2025-12-08 18:49:50 +0100

    refactor: improve formatting and consistency in MockServer and MockSessionManager

--- server/tests/game/MockServer.hpp
+++ server/tests/game/MockServer.hpp
@@ -9,26 +9,56 @@
 #include "server/interfaces/IServer.hpp"
 
 class MockServer : public Net::Server::IServer {
-public:
+  public:
     bool sent = false;
 
-    void configure(const std::string&, uint16_t) override {}
-    void setNonBlocking(bool) noexcept override {}
-    bool isStoredIpCorrect() const noexcept override { return true; }
-    bool isStoredPortCorrect() const noexcept override { return true; }
+    void configure(const std::string &, uint16_t) override
+    {
+    }
 
-    bool sendPacket(const Net::IPacket &) override {
+    void setNonBlocking(bool) noexcept override
+    {
+    }
+
+    bool isStoredIpCorrect() const noexcept override
+    {
+        return true;
+    }
+
+    bool isStoredPortCorrect() const noexcept override
+    {
+        return true;
+    }
+
+    bool sendPacket(const Net::IPacket &) override
+    {
         sent = true;
         return true;
     }
 
-    void start() override {}
-    void stop() override {}
-    bool isRunning() const noexcept override { return false; }
-    void setRunning(bool) noexcept override {}
-    void readPackets() override {}
+    void start() override
+    {
+    }
 
-    bool popPacket(std::shared_ptr<Net::IPacket>&) override {
+    void stop() override
+    {
+    }
+
+    bool isRunning() const noexcept override
+    {
+        return false;
+    }
+
+    void setRunning(bool) noexcept override
+    {
+    }
+
+    void readPackets() override
+    {
+    }
+
+    bool popPacket(std::shared_ptr<Net::IPacket> &) override
+    {
         return false;
     }
 };
--- server/tests/game/MockSessionManager.hpp
+++ server/tests/game/MockSessionManager.hpp
@@ -7,21 +7,23 @@
 
 #pragma once
 
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include "network/SessionManager/SessionManager.hpp"
+#include <netinet/in.h>
+#include "ISessionManager.hpp"
 
-class MockSessionManager : public Net::Server::SessionManager {
-public:
-    sockaddr_in fakeAddr{};
+class MockSessionManager : public Net::Server::ISessionManager {
+  public:
+    Net::Server::AddressIn fakeAddr{};
 
-    MockSessionManager() {
+    MockSessionManager()
+    {
         fakeAddr.sin_family = AF_INET;
         fakeAddr.sin_port = htons(1234);
         inet_pton(AF_INET, "127.0.0.1", &fakeAddr.sin_addr);
     }
 
-    const sockaddr_in *getAddress(int) const {
+    const adressIn *getAddress(int) const
+    {
         return &fakeAddr;
     }
 };

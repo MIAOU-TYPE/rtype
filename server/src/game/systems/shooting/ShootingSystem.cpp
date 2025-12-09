f4bb78af6de7ce57e2a38fa49eb7217efa3a05fb
ann7415 <anna.poghosyan@epitech.eu>
2025-12-09 16:30:48 +0100

    refactor: streamline shooting logic in ShootingSystem for improved readability

--- server/src/game/systems/shooting/ShootingSystem.cpp
+++ server/src/game/systems/shooting/ShootingSystem.cpp
@@ -13,16 +13,15 @@ namespace Game
     {
         auto &reg = world.registry();
 
-        reg.view<InputComponent, Ecs::Position>(
-            [&reg](Ecs::Entity, InputComponent &input, const Ecs::Position &pos) {
-                if (!input.shoot)
-                    return;
-                input.shoot = false;
-                const Ecs::Entity proj = reg.createEntity();
-                reg.emplaceComponent<Ecs::Position>(proj, pos.x + 20.f, pos.y);
-                reg.emplaceComponent<Ecs::Velocity>(proj, 400.f, 0.f);
-                reg.emplaceComponent<Ecs::Damage>(proj, 20);
-                reg.emplaceComponent<Ecs::Collision>(proj, 8.f, 8.f);
-            });
+        reg.view<InputComponent, Ecs::Position>([&reg](Ecs::Entity, InputComponent &input, const Ecs::Position &pos) {
+            if (!input.shoot)
+                return;
+            input.shoot = false;
+            const Ecs::Entity proj = reg.createEntity();
+            reg.emplaceComponent<Ecs::Position>(proj, pos.x + 20.f, pos.y);
+            reg.emplaceComponent<Ecs::Velocity>(proj, 400.f, 0.f);
+            reg.emplaceComponent<Ecs::Damage>(proj, 20);
+            reg.emplaceComponent<Ecs::Collision>(proj, 8.f, 8.f);
+        });
     }
 } // namespace Game

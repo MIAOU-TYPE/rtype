b4449cd5f977f7e76182cfe2ea8a6a180500d9d7
ann7415 <anna.poghosyan@epitech.eu>
2025-12-09 16:47:15 +0100

    feat: implement AISystem to manage AI-controlled entity behavior

---
+++ server/src/game/systems/AI/AISystem.cpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** AISystem
+*/
+
+#include "AISystem.hpp"
+#include <cstdlib>
+
+namespace Game
+{
+    void AISystem::update(IGameWorld &world, float dt)
+    {
+        auto &reg = world.registry();
+
+        reg.view<Ecs::AIBrain, Ecs::Velocity>([dt](Ecs::Entity, Ecs::AIBrain &brain, Ecs::Velocity &vel) {
+            brain.timer += dt;
+
+            switch (brain.state) {
+                case Ecs::AIState::Idle:
+                    vel.vx = -20.f;
+                    if (brain.timer > 1.f) {
+                        brain.timer = 0.f;
+                        brain.state = Ecs::AIState::Patrol;
+                    }
+                    break;
+
+                case Ecs::AIState::Patrol:
+                    vel.vx = -40.f + (rand() % 20 - 10);
+                    vel.vy = (rand() % 50 - 25);
+
+                    if (brain.timer > 2.f) {
+                        brain.timer = 0.f;
+                        brain.state = Ecs::AIState::Idle;
+                    }
+                    break;
+
+                default: break;
+            }
+        });
+    }
+} // namespace Game

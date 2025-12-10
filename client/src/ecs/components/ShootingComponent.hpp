bc51f9a6429ba93f82c3e53cf52a1c2a8b63eca0
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-12-10 13:57:12 +0100

    refactor: standardize comment formatting in ShootingComponent

--- client/src/ecs/components/ShootingComponent.hpp
+++ client/src/ecs/components/ShootingComponent.hpp
@@ -14,8 +14,8 @@ namespace Ecs
      * @brief Marks an entity as wanting to shoot.
      */
     struct ShootingComponent {
-        bool wantsToShoot = false; /// True when the entity wants to fire and cooldown has elapsed
-        float cooldown = 0.f;    /// Time remaining until the next shot can be fired
-        float fireRate = 0.25f; /// Time between shots in seconds
+        bool wantsToShoot = false;  ///> True when the entity wants to fire and cooldown has elapsed
+        float cooldown = 0.f;       ///> Time remaining until the next shot can be fired
+        float fireRate = 0.25f;     ///> Time between shots in seconds
     };
 } // namespace Ecs

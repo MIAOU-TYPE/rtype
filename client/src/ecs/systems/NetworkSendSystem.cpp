6977b11fc9aac8c366cfd2ad65c48d4c337a0071
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-12-10 17:14:38 +0100

    feat: add NetworkSendSystem for handling input packet transmission in ECS

---
+++ client/src/ecs/systems/NetworkSendSystem.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** NetworkSystem
+*/
+
+#include "NetworkSendSystem.hpp"
+
+namespace Ecs
+{
+
+    NetworkSendSystem::NetworkSendSystem(std::shared_ptr<Registry> registry, std::shared_ptr<Game::NetClient> netClient)
+        : _registry(registry), _netClient(netClient)
+    {
+    }
+
+    void NetworkSendSystem::update(float deltaTime)
+    {
+         _registry->view<Game::InputComponent>([this](Entity entity, const Game::InputComponent &input) {
+            float dx = 0.0f;
+            float dy = 0.0f;
+            
+            if (input.left)
+                dx -= 1.0f;
+            if (input.right)
+                dx += 1.0f;
+            if (input.up)
+                dy -= 1.0f;
+            if (input.down)
+                dy += 1.0f;
+            
+            _netClient->sendInputPacket(dx, dy, input.shoot);
+        });
+    }
+
+} // namespace Ecs

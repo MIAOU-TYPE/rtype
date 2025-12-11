cb76b2375a30502d1c1820b71851ac32c3532cb6
ann7415 <anna.poghosyan@epitech.eu>
2025-12-11 11:54:54 +0100

    fix: adjust sleep duration in ServerRuntime for improved game loop timing

--- server/src/thread/ServerRuntime.cpp
+++ server/src/thread/ServerRuntime.cpp
@@ -80,6 +80,6 @@ void ServerRuntime::runUpdate() const
 {
     while (_running) {
         _gameServer->tick();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
 }

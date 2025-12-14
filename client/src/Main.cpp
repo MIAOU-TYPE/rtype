b9499e4bfcd66367265b8346d70895cbb2a5796d
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-12-14 21:57:37 +0100

    fix: ensure runtime.wait() is called after starting ClientRuntime

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -15,6 +15,7 @@ int main()
     try {
         Thread::ClientRuntime runtime(std::make_shared<Network::NetClient>());
         runtime.start();
+        runtime.wait();
         return 0;
     } catch (const std::exception &e) {
         std::cerr << "R-Type Client System Error: " << e.what() << std::endl;

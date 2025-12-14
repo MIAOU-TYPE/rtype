1c2e5e1719702de77c546355ea9107f5d0aa129e
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-12-14 19:57:43 +0100

    refactor: reorganize ClientRuntime class structure and improve header file includes

--- client/src/thread/ClientRuntime.cpp
+++ client/src/thread/ClientRuntime.cpp
@@ -7,52 +7,53 @@
 
 #include "ClientRuntime.hpp"
 
-namespace Thread {
-
-ClientRuntime::ClientRuntime(const std::shared_ptr<Network::NetClient> &client) : _client(client)
+namespace Thread
 {
-}
 
-ClientRuntime::~ClientRuntime()
-{
-    std::scoped_lock lock(_mutex);
-    if (!_stopRequested)
-        stop();
-    _client = nullptr;
-}
+    ClientRuntime::ClientRuntime(const std::shared_ptr<Network::NetClient> &client) : _client(client)
+    {
+    }
 
-void ClientRuntime::wait()
-{
-    std::unique_lock lock(_mutex);
-    _cv.wait(lock, [this]() {
-        return _stopRequested;
-    });
-}
-
-void ClientRuntime::start()
-{
-    _running = true;
-    _receiverThread = std::thread(&ClientRuntime::runReceiver, this);
-}
-
-void ClientRuntime::stop()
-{
+    ClientRuntime::~ClientRuntime()
     {
         std::scoped_lock lock(_mutex);
-        _stopRequested = true;
-        _running = false;
+        if (!_stopRequested)
+            stop();
+        _client = nullptr;
     }
-    _cv.notify_all();
 
-    if (_receiverThread.joinable())
-        _receiverThread.join();
-}
-
-void ClientRuntime::runReceiver() const
-{
-    while (_running) {
-        _client->receivePackets();
+    void ClientRuntime::wait()
+    {
+        std::unique_lock lock(_mutex);
+        _cv.wait(lock, [this]() {
+            return _stopRequested;
+        });
     }
-}
+
+    void ClientRuntime::start()
+    {
+        _running = true;
+        _receiverThread = std::thread(&ClientRuntime::runReceiver, this);
+    }
+
+    void ClientRuntime::stop()
+    {
+        {
+            std::scoped_lock lock(_mutex);
+            _stopRequested = true;
+            _running = false;
+        }
+        _cv.notify_all();
+
+        if (_receiverThread.joinable())
+            _receiverThread.join();
+    }
+
+    void ClientRuntime::runReceiver() const
+    {
+        while (_running) {
+            _client->receivePackets();
+        }
+    }
 
 } // namespace Thread
--- client/src/thread/ClientRuntime.hpp
+++ client/src/thread/ClientRuntime.hpp
@@ -7,36 +7,37 @@
 
 #pragma once
 
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <atomic>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
 
 #include "NetClient.hpp"
 
-namespace Thread {
+namespace Thread
+{
 
-/**
- * @brief The ClientRuntime class is responsible for managing the client's runtime operations,
- * including starting, stopping, and handling incoming packets.
- */
-class ClientRuntime {
-public:
-    /** 
-     * @brief Constructor for ClientRuntime.
-     * @param client Shared pointer to the network client.
+    /**
+     * @brief The ClientRuntime class is responsible for managing the client's runtime operations,
+     * including starting, stopping, and handling incoming packets.
      */
-    explicit ClientRuntime(const std::shared_ptr<Network::NetClient> &client);
-    ~ClientRuntime();
+    class ClientRuntime {
+      public:
+        /**
+         * @brief Constructor for ClientRuntime.
+         * @param client Shared pointer to the network client.
+         */
+        explicit ClientRuntime(const std::shared_ptr<Network::NetClient> &client);
+        ~ClientRuntime();
 
-    void start();
-    void stop();
-    void wait();
+        void start();
+        void stop();
+        void wait();
 
-private:
+      private:
         std::shared_ptr<Network::NetClient> _client; ///> Network client
 
-        std::thread _receiverThread;  ///> Thread for receiving packets
+        std::thread _receiverThread; ///> Thread for receiving packets
         // std::thread _updateThread;    ///> Thread for updating game state
         // std::thread _renderThread;    ///> Thread for rendering graphics
         // std::thread _inputThread;     ///> Thread for handling user input
@@ -46,7 +47,7 @@ private:
         bool _stopRequested = false;       ///> Flag to indicate if a stop has been requested
         std::atomic<bool> _running{false}; ///> Atomic flag to indicate if the server is running
 
-        void runReceiver() const;          ///> Method for running the receiver thread
-};
+        void runReceiver() const; ///> Method for running the receiver thread
+    };
 
 } // namespace Thread

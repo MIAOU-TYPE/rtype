72c50823d037e990f86a4118a139d4e1714d8264
ann7415 <anna.poghosyan@epitech.eu>
2025-12-15 12:19:20 +0100

    test: refactor GameServer tests to use JSON level configuration

--- server/tests/game/gameServer/testGameServer.cpp
+++ server/tests/game/gameServer/testGameServer.cpp
@@ -6,11 +6,7 @@
 */
 
 #include <gtest/gtest.h>
-
-#define private public
 #include "GameServer.hpp"
-#undef private
-
 #include "MockServer.hpp"
 #include "MockSessionManager.hpp"
 #include "UDPPacket.hpp"
@@ -18,29 +14,16 @@
 TEST(GameServer, creates_player_on_connect)
 {
     auto sessions = std::make_shared<MockSessionManager>();
-    auto server = std::make_shared<MockServer>();
-    auto factory = std::make_shared<Net::Factory::PacketFactory>(std::make_shared<Net::UDPPacket>());
+    auto server   = std::make_shared<MockServer>();
+    auto factory  = std::make_shared<Net::Factory::PacketFactory>(
+        std::make_shared<Net::UDPPacket>());
 
-    Game::GameServer gs(sessions, server, factory);
-
-    ASSERT_TRUE(gs._levelManager.load(R"(
-    {
-      "name": "TestLevel",
-      "duration": 10,
-      "enemies": {
-        "small": {
-          "hp": 10,
-          "speed": -50,
-          "size": { "w": 10, "h": 10 }
-        }
-      },
-      "waves": [
-        { "time": 1.0, "enemies": { "small": 2 } }
-      ]
-    }
-    )"));
-
-    gs._levelManager.reset();
+    Game::GameServer gs(
+        sessions,
+        server,
+        factory,
+        "game/levels/test_level.json"
+    );
 
     gs.onPlayerConnect(42);
 
@@ -48,36 +31,22 @@ TEST(GameServer, creates_player_on_connect)
     input.right = true;
     gs.onPlayerInput(42, input);
 
-    gs.update(1.0f);
-    SUCCEED();
+    EXPECT_NO_THROW(gs.update(1.0f));
 }
 
 TEST(GameServer, destroys_player_on_disconnect)
 {
     auto sessions = std::make_shared<MockSessionManager>();
-    auto server = std::make_shared<MockServer>();
-    auto factory = std::make_shared<Net::Factory::PacketFactory>(std::make_shared<Net::UDPPacket>());
+    auto server   = std::make_shared<MockServer>();
+    auto factory  = std::make_shared<Net::Factory::PacketFactory>(
+        std::make_shared<Net::UDPPacket>());
 
-    Game::GameServer gs(sessions, server, factory);
-
-    ASSERT_TRUE(gs._levelManager.load(R"(
-    {
-      "name": "TestLevel",
-      "duration": 10,
-      "enemies": {
-        "small": {
-          "hp": 10,
-          "speed": -50,
-          "size": { "w": 10, "h": 10 }
-        }
-      },
-      "waves": [
-        { "time": 1.0, "enemies": { "small": 1 } }
-      ]
-    }
-    )"));
-
-    gs._levelManager.reset();
+    Game::GameServer gs(
+        sessions,
+        server,
+        factory,
+        "game/levels/test_level.json"
+    );
 
     gs.onPlayerConnect(1);
     gs.onPlayerDisconnect(1);

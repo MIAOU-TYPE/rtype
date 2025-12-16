9db2470fa689dc811fc1bff7de19bbc24a96fc45
ann7415 <anna.poghosyan@epitech.eu>
2025-12-16 01:42:49 +0100

    refactor: enhance documentation for NetworkIdentity and NetworkInterpolation structs

--- client/src/ecs/components/NetworkIdentity.hpp
+++ client/src/ecs/components/NetworkIdentity.hpp
@@ -10,8 +10,12 @@
 
 namespace Ecs
 {
+    /**
+     * @struct NetworkIdentity
+     * @brief Component that holds network identity information for an entity.
+     */
     struct NetworkIdentity {
-        uint64_t id;
-        bool authoritative = false;
+        uint64_t id;                     ///> Unique network ID
+        bool authoritative = false;      ///> Is this entity authoritative
     };
-}
+} // namespace Ecs
--- client/src/ecs/components/NetworkInterpolation.hpp
+++ client/src/ecs/components/NetworkInterpolation.hpp
@@ -9,13 +9,17 @@
 
 namespace Ecs
 {
+    /**
+     * @struct NetworkInterpolation
+     * @brief Component that holds network interpolation data for an entity.
+     */
     struct NetworkInterpolation {
-        float prevX = 0.f;
-        float prevY = 0.f;
-        float targetX = 0.f;
-        float targetY = 0.f;
+        float prevX = 0.f;      ///> Previous X position
+        float prevY = 0.f;      ///> Previous Y position
+        float targetX = 0.f;    ///> Target X position
+        float targetY = 0.f;    ///> Target Y position
 
-        float alpha = 1.f;
-        float snapshotInterval = 0.05f;
+        float alpha = 1.f;              ///> Interpolation factor
+        float snapshotInterval = 0.05f; ///> Time between snapshots
     };
-}
+} // namespace Ecs

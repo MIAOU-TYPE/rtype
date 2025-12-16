e7706601b3eafb88e1920fc3b513c013efe7c906
ann7415 <anna.poghosyan@epitech.eu>
2025-12-16 06:51:11 +0100

    feat: add RenderLink and SpriteTag structures for entity rendering

---
+++ client/src/ecs/components/RenderLink.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** RenderLink
+*/
+
+#pragma once
+#include <memory>
+#include "SFMLEntityCreation.hpp"
+
+namespace Ecs {
+    struct RenderLink {
+        std::shared_ptr<Graphics::GraphicalEntity> gfx;
+    };
+}
---
+++ client/src/ecs/components/SpriteTag.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** SpriteTag
+*/
+
+#pragma once
+#include <string>
+
+namespace Ecs {
+    struct SpriteTag {
+        std::string name;
+    };
+}

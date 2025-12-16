4832be5491a2531ab93860d130327ecc7de66daf
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-12-16 03:04:43 +0100

    feat: add ColorBlindType enumeration for color blindness support

---
+++ client/src/graphics/ColorBlindType.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** ColorBlindType
+*/
+
+#pragma once
+
+namespace Graphics {
+    /**
+     * @enum ColorBlindType
+     * @brief Enumeration for different types of color blindness.
+     */
+    enum class ColorBlindType {
+        NONE,
+        PROTANOPIA,
+        DEUTERANOPIA,
+        TRITANOPIA
+    };
+} // namespace Graphics

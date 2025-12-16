f5f96a07bede623f570324dcdd15f040faf04b6a
romain1717 <romain.berard@epitech.eu>
2025-12-16 13:18:38 +0100

    feat: adjust projectile position and velocity in ShootingSystem for improved gameplay dynamics

--- server/src/game/systems/shooting/ShootingSystem.cpp
+++ server/src/game/systems/shooting/ShootingSystem.cpp
@@ -18,8 +18,8 @@ namespace Game
                 return;
             input.shoot = false;
             const Ecs::Entity proj = reg.createEntity();
-            reg.emplaceComponent<Ecs::Position>(proj, pos.x + 20.f, pos.y);
-            reg.emplaceComponent<Ecs::Velocity>(proj, 400.f, 0.f);
+            reg.emplaceComponent<Ecs::Position>(proj, pos.x, pos.y);
+            reg.emplaceComponent<Ecs::Velocity>(proj, 15.f, 0.f);
             reg.emplaceComponent<Ecs::Damage>(proj, 20);
             reg.emplaceComponent<Ecs::Collision>(proj, 8.f, 8.f);
         });

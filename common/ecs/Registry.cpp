20a73d57bfa1a7414061e1a4dc4b30389b0f41a5
ann7415 <anna.poghosyan@epitech.eu>
2025-12-19 18:27:09 +0100

    feat: implement Registry class methods for entity management in Ecs

---
+++ common/ecs/Registry.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** Registry
+*/
+
+#include "Registry.hpp"
+
+namespace Ecs
+{
+    Entity Registry::createEntity() noexcept
+    {
+        const Entity entity(_entityCounter);
+        _entityCounter++;
+        return entity;
+    }
+
+    void Registry::destroyEntity(const Entity entity) noexcept
+    {
+        try {
+            for (auto &func : _destroyers)
+                func(*this, entity);
+        } catch (...) {
+            return;
+        }
+    }
+
+    void Registry::clear() noexcept
+    {
+        _entityCounter = 0;
+        _entityToIndex.clear();
+        _destroyers.clear();
+    }
+} // namespace Ecs

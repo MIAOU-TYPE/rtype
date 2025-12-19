57950c37122c4f97c3414b7cc7f2faee79539d19
romain1717 <romain.berard@epitech.eu>
2025-12-19 23:40:59 +0100

    feat: refactor main function by removing unused includes and simplifying argument parsing

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -4,51 +4,7 @@
 ** File description:
 ** Main
 */
-#include "ArgParser.hpp"
-#include "ClientRuntime.hpp"
-#include "GameScene.hpp"
-#include "NetClient.hpp"
-#include "SFMLRenderer.hpp"
-#include "SFMLTextureManager.hpp"
-#include "SignalHandler.hpp"
-
-static std::shared_ptr<Signal::SignalHandler> startSignalHandler(Thread::ClientRuntime &runtime)
-{
-    std::shared_ptr<Signal::SignalHandler> signalHandler = std::make_shared<Signal::SignalHandler>();
-
-    signalHandler->start();
-    signalHandler->registerCallback(Signal::SignalType::Interrupt, [&runtime]() {
-        runtime.stop();
-    });
-    return signalHandler;
-}
 
 int main(int argc, char **argv)
 {
-    Utils::ArgParser parser(argc, argv);
-    if (const Utils::ArgParseResult result = parser.parse(); result == Utils::ArgParseResult::HelpDisplayed) {
-        return 0;
-    } else if (result == Utils::ArgParseResult::Error) {
-        std::cerr << "{main}: Error parsing arguments. Use --help for usage information." << std::endl;
-        return 84;
-    }
-    const int port = parser.getPort();
-    const std::string host = parser.getHost();
-    try {
-        auto renderer = std::make_shared<Graphics::SFMLRenderer>();
-        auto textureManager = std::make_shared<Graphics::SFMLTextureManager>();
-        const auto client = std::make_shared<Network::NetClient>();
-
-        Thread::ClientRuntime runtime(client);
-        const auto signalHandler = startSignalHandler(runtime);
-
-        client->configure(host, port);
-        runtime.start();
-        runtime.wait();
-        signalHandler->stop();
-        return 0;
-    } catch (const std::exception &e) {
-        std::cerr << "{main}: " << e.what() << std::endl;
-        return 1;
-    }
 }
--- client/src/graphics/interfaces/ISpriteFactory.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ISPRITE_FACTORY_HPP
-#define ISPRITE_FACTORY_HPP
-
-// ISpriteFactory interface
-
-#endif // ISPRITE_FACTORY_HPP

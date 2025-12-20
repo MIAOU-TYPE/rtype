e1dec349c2cc7eef39a11a50f7f5731cf186545c
romain1717 <romain.berard@epitech.eu>
2025-12-20 07:45:51 +0100

    feat: add AssetLoader for loading sprites and managing texture handles

---
+++ client/src/utils/assetLoader/AssetLoader.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** assetLoader
+*/
+
+#include "AssetLoader.hpp"
+#include "SpriteLoader.hpp"
+
+namespace Utils
+{
+    void AssetLoader::load(std::shared_ptr<Graphics::ITextureManager> textureManager,
+        const std::shared_ptr<Engine::SpriteRegistry> &spriteRegistry)
+    {
+        Engine::SpriteLoader::loadFromFile("client/assets/json/boss.json", *spriteRegistry);
+
+        Engine::SpriteLoader::loadFromFile("client/assets/json/enemy.json", *spriteRegistry);
+        Engine::SpriteLoader::loadFromFile("client/assets/json/enemy2.json", *spriteRegistry);
+        Engine::SpriteLoader::loadFromFile("client/assets/json/enemy3.json", *spriteRegistry);
+
+        Engine::SpriteLoader::loadFromFile("client/assets/json/explose.json", *spriteRegistry);
+
+        Engine::SpriteLoader::loadFromFile("client/assets/json/missile.json", *spriteRegistry);
+
+        Engine::SpriteLoader::loadFromFile("client/assets/json/player.json", *spriteRegistry);
+        Engine::SpriteLoader::loadFromFile("client/assets/json/player2.json", *spriteRegistry);
+        Engine::SpriteLoader::loadFromFile("client/assets/json/player3.json", *spriteRegistry);
+        Engine::SpriteLoader::loadFromFile("client/assets/json/player4.json", *spriteRegistry);
+
+        spriteRegistry->forEach([&](const std::string &, const Engine::SpriteDefinition &def) {
+            const Graphics::TextureHandle handle = textureManager->load(def.texturePath);
+            auto &mutableDef = const_cast<Engine::SpriteDefinition &>(def);
+            mutableDef.textureHandle = handle;
+        });
+    }
+} // namespace Utils
---
+++ client/src/utils/assetLoader/AssetLoader.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** assetLoader
+*/
+
+#pragma once
+#include "ITextureManager.hpp"
+#include "SpriteRegistry.hpp"
+
+namespace Utils
+{
+    class AssetLoader {
+      public:
+        static void load(std::shared_ptr<Graphics::ITextureManager> textureManager,
+            const std::shared_ptr<Engine::SpriteRegistry> &spriteRegistry);
+    };
+} // namespace Utils

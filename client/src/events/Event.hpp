b693543be14233757178425305bc9bcc4a074354
romain1717 <romain.berard@epitech.eu>
2025-12-20 01:49:54 +0100

    feat: implement centralized event bus with key and mouse event handling

---
+++ client/src/events/Event.hpp
@@ -0,0 +1,91 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Event
+*/
+
+#pragma once
+#include "Key.hpp"
+
+namespace Core
+{
+    /**
+     * @brief Base class for all events.
+     */
+    struct Event {
+        /**
+         * @brief Virtual destructor for Event.
+         */
+        virtual ~Event() = default;
+    };
+
+    /**
+     * @brief Event triggered when the window is closed.
+     */
+    struct WindowClosed : Event {};
+
+    /**
+     * @brief Event triggered when a key is pressed.
+     */
+    struct KeyPressed : Event {
+        /**
+         * @brief Constructor for KeyPressed event.
+         * @param k The key that was pressed.
+         */
+        explicit KeyPressed(const Key k) : key(k)
+        {
+        }
+
+        Key key; ///> The key that was pressed.
+    };
+
+    /**
+     * @brief Event triggered when a key is released.
+     */
+    struct KeyReleased : Event {
+        /**
+         * @brief Constructor for KeyReleased event.
+         * @param k The key that was released.
+         */
+        explicit KeyReleased(const Key k) : key(k)
+        {
+        }
+
+        Key key; ///> The key that was released.
+    };
+
+    /**
+     * @brief Event triggered when a mouse button is pressed.
+     */
+    struct MousePressed : Event {
+        /**
+         * @brief Constructor for MousePressed event.
+         * @param k The mouse button that was pressed.
+         * @param x The x position of the mouse when pressed.
+         * @param y The y position of the mouse when pressed.
+         */
+        explicit MousePressed(const Key k, const int x, const int y) : Key(k), posX(x), posY(y)
+        {
+        }
+
+        Key Key;         ///> The mouse button that was pressed.
+        int posX = 0.0f; ///> The x position of the mouse when pressed.
+        int posY = 0.0f; ///> The y position of the mouse when
+    };
+
+    /**
+     * @brief Event triggered when a mouse button is released.
+     */
+    struct MouseReleased : Event {
+        /**
+         * @brief Constructor for MouseReleased event.
+         * @param k The mouse button that was released.
+         */
+        explicit MouseReleased(const Key k) : Key(k)
+        {
+        }
+
+        Key Key; ///> The mouse button that was released.
+    };
+} // namespace Core

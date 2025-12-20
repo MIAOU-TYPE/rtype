7eec51748f1f201b8fca0d0df66368d7e37a5a9b
ann7415 <anna.poghosyan@epitech.eu>
2025-12-20 02:19:25 +0100

    feat: add AnimationState component to manage animation states for entities

---
+++ client/src/ecs/components/AnimationState.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** AnimationState
+*/
+
+#pragma once
+#include <cstddef>
+#include <string>
+
+namespace Engine
+{
+    /**
+    ** @brief Component that holds the state of an animation for an entity.
+    */
+    struct AnimationState {
+        std::string currentAnimation;   ///> Name of the current animation.
+        std::size_t frameIndex = 0;      ///> Current frame index in the animation.
+        float elapsed = 0.f;    ///> Time elapsed since the last frame change.
+    };
+} // namespace Engine

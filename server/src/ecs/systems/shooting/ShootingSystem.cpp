ff7e32237b8c76d2a4b96348dc1432839dfd8815
romain1717 <romain.berard@epitech.eu>
2025-12-21 04:45:21 +0100

    feat: enhance ShootingSystem to include Damageable and Drawable components for projectiles

--- server/src/ecs/systems/shooting/ShootingSystem.cpp
+++ server/src/ecs/systems/shooting/ShootingSystem.cpp
@@ -18,10 +18,12 @@ namespace Game
                 return;
             input.shoot = false;
             const Ecs::Entity proj = reg.createEntity();
-            reg.emplaceComponent<Ecs::Position>(proj, pos.x, pos.y);
-            reg.emplaceComponent<Ecs::Velocity>(proj, 15.f, 0.f);
+            reg.emplaceComponent<Ecs::Position>(proj, pos.x + 20, pos.y);
+            reg.emplaceComponent<Ecs::Velocity>(proj, 100.f, 0.f);
             reg.emplaceComponent<Ecs::Damage>(proj, 20);
+            reg.emplaceComponent<Ecs::Damageable>(proj);
             reg.emplaceComponent<Ecs::Collision>(proj, 8.f, 8.f);
+            reg.emplaceComponent<Ecs::Drawable>(proj, 6);
         });
     }
 } // namespace Game

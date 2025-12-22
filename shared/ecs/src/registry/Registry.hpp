a61b42ca0ef28ea726188d17ab408517a585f5aa
romain1717 <romain.berard@epitech.eu>
2025-12-22 04:23:57 +0100

    fix: correct formatting of comment in const view method in Registry

--- shared/ecs/src/registry/Registry.hpp
+++ shared/ecs/src/registry/Registry.hpp
@@ -104,8 +104,8 @@ namespace Ecs
         void view(Function fn);
 
         /**
-        * @brief Const version of view to iterate over entities owning a set of components.
-        * Only entities with all specified components will be passed to the function.
+         * @brief Const version of view to iterate over entities owning a set of components.
+         * Only entities with all specified components will be passed to the function.
          */
         template <typename... Components, typename Function>
         void view(Function fn) const;

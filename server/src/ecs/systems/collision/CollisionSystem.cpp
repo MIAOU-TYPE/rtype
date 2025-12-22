c00fd6131df24e1b996ce89c482c467ade7c0fc4
romain1717 <romain.berard@epitech.eu>
2025-12-22 21:57:40 +0100

    fix: improve collision detection logic and include AIBrain component in CollisionSystem

--- server/src/ecs/systems/collision/CollisionSystem.cpp
+++ server/src/ecs/systems/collision/CollisionSystem.cpp
@@ -7,13 +7,16 @@
 
 #include "CollisionSystem.hpp"
 
-namespace Game
+namespace
 {
-    static bool intersects(
-        const Ecs::Position &a, const Ecs::Collision &ac, const Ecs::Position &b, const Ecs::Collision &bc)
+    bool intersects(const Ecs::Position &a, const Ecs::Collision &ac, const Ecs::Position &b, const Ecs::Collision &bc)
     {
         return !(a.x > b.x + bc.width || a.x + ac.width < b.x || a.y > b.y + bc.height || a.y + ac.height < b.y);
     }
+} // namespace
+
+namespace Game
+{
 
     void CollisionSystem::update(IGameWorld &world)
     {
@@ -23,29 +26,26 @@ namespace Game
         auto &colArr = reg.getComponents<Ecs::Collision>();
         auto &dmgArr = reg.getComponents<Ecs::Damage>();
         auto &hpArr = reg.getComponents<Ecs::Health>();
-
-        std::vector<Ecs::Entity> toDestroy;
+        auto &iaBrainArr = reg.getComponents<Ecs::AIBrain>();
 
         for (size_t i = 0; i < posArr.size(); i++) {
             if (!posArr.at(i) || !colArr.at(i))
                 continue;
 
             for (size_t j = i + 1; j < posArr.size(); j++) {
+                if (iaBrainArr.at(j) && iaBrainArr.at(i))
+                    continue;
                 if (!posArr.at(j) || !colArr.at(j))
                     continue;
                 if (!intersects(*posArr.at(i), *colArr.at(i), *posArr.at(j), *colArr.at(j)))
                     continue;
                 if (dmgArr.at(i) && hpArr.at(j)) {
                     hpArr.at(j)->hp -= dmgArr.at(i)->amount;
-                    toDestroy.emplace_back(i);
-                } else if (dmgArr.at(j) && hpArr.at(i)) {
+                }
+                if (dmgArr.at(j) && hpArr.at(i)) {
                     hpArr.at(i)->hp -= dmgArr.at(j)->amount;
-                    toDestroy.emplace_back(j);
                 }
             }
         }
-
-        for (auto ent : toDestroy)
-            reg.destroyEntity(ent);
     }
 } // namespace Game

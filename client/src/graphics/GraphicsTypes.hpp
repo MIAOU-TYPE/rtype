72f6a138d4b7ef8f3da38688a7b005f221b2e1e5
ann7415 <anna.poghosyan@epitech.eu>
2025-12-22 02:59:52 +0100

    feat: add GraphicsTypes.hpp with ViewportSize struct definition

---
+++ client/src/graphics/GraphicsTypes.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** GraphicsTypes
+*/
+
+#pragma once
+
+namespace Graphics
+{
+    /**
+     * @struct ViewportSize
+     * @brief Represents the size of a viewport with width and height.
+     */
+    struct ViewportSize {
+        unsigned int width; ///> The width of the viewport
+        unsigned int height;    ///> The height of the viewport
+    };
+}

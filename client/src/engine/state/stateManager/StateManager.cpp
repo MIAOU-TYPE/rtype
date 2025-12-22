c4577ff33541bcb94f4ee63af2b128870e69726c
ann7415 <anna.poghosyan@epitech.eu>
2025-12-22 03:27:23 +0100

    feat: implement StateManager for managing game states

---
+++ client/src/engine/state/stateManager/StateManager.cpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** StateManager
+*/
+
+#include "StateManager.hpp"
+
+namespace Engine
+{
+    void StateManager::changeState(std::unique_ptr<IGameState> state)
+    {
+        if (_current)
+            _current->onExit();
+        _current = std::move(state);
+        if (_current)
+            _current->onEnter(*this);
+        else
+            _running = false;
+    }
+
+    void StateManager::update(const float dt) const
+    {
+        if (_current)
+            _current->update(dt);
+    }
+
+    void StateManager::render() const
+    {
+        if (_current)
+            _current->render();
+    }
+
+    bool StateManager::isRunning() const
+    {
+        return _running;
+    }
+}
---
+++ client/src/engine/state/stateManager/StateManager.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** StateManager
+*/
+
+#pragma once
+
+#include <memory>
+#include "IGameState.hpp"
+
+namespace Engine
+{
+    /**
+     * @brief Manages the different states of the game.
+     */
+    class StateManager {
+    public:
+        /**
+         * @brief Construct a new State Manager object with an initial state.
+         *
+         * @param state The initial game state.
+         */
+        void changeState(std::unique_ptr<IGameState> state);
+
+        /**
+         * @brief Update the current state.
+         *
+         * @param dt Delta time since last update.
+         */
+        void update(float dt) const;
+
+        /**
+         * @brief Render the current state.
+         */
+        void render() const;
+
+        /**
+         * @brief Check if the state manager is running.
+         *
+         * @return true if running, false otherwise.
+         */
+        bool isRunning() const;
+
+    private:
+        std::unique_ptr<IGameState> _current;   ///> The current game state.
+        bool _running = true;       ///> Indicates if the state manager is running.
+    };
+}

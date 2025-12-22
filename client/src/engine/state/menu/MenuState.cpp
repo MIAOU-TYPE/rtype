47a02325ee00eab2179415dcaa6de70190e5cf96
ann7415 <anna.poghosyan@epitech.eu>
2025-12-22 03:27:44 +0100

    feat: implement MenuState class for managing the game menu

---
+++ client/src/engine/state/menu/MenuState.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** MenuState
+*/
+
+#include "MenuState.hpp"
+
+namespace Engine
+{
+    MenuState::MenuState(
+        std::shared_ptr<Graphics::IGraphics> graphics,
+        std::shared_ptr<Graphics::IRenderer> renderer)
+        : _graphics(std::move(graphics)), _renderer(std::move(renderer))
+    {
+    }
+
+    void MenuState::onEnter(StateManager &manager)
+    {
+        _manager = &manager;
+        _menu = std::make_unique<Menu>(_renderer);
+    }
+
+    void MenuState::onExit()
+    {
+        _menu.reset();
+    }
+
+    void MenuState::update(const float dt) {
+        _menu->update(dt);
+    }
+
+    void MenuState::render() {
+        _menu->render();
+    }
+}
---
+++ client/src/engine/state/menu/MenuState.hpp
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** MenuState
+*/
+
+#pragma once
+
+#include "IGameState.hpp"
+#include "Menu.hpp"
+#include "IRenderer.hpp"
+#include "IGraphics.hpp"
+
+namespace Engine
+{
+    /**
+     * @brief Represents the menu state of the game.
+     */
+    class MenuState final : public IGameState {
+    public:
+        /**
+         * @brief Construct a new Menu State object.
+         *
+         * @param graphics Shared pointer to the graphics interface.
+         * @param renderer Shared pointer to the renderer interface.
+         */
+        explicit MenuState(
+            std::shared_ptr<Graphics::IGraphics> graphics,
+            std::shared_ptr<Graphics::IRenderer> renderer);
+
+        /**
+         * @brief Destroy the Menu State object.
+         */
+        void onEnter(StateManager &manager) override;
+
+        /**
+         * @brief Clean up resources when exiting the menu state.
+         */
+        void onExit() override;
+
+        /**
+         * @brief Update the menu state.
+         *
+         * @param dt Delta time since last update.
+         */
+        void update(float dt) override;
+
+        /**
+         * @brief Render the menu state.
+         */
+        void render() override;
+
+    private:
+        std::shared_ptr<Graphics::IGraphics> _graphics; ///> Shared pointer to the graphics interface.
+        std::shared_ptr<Graphics::IRenderer> _renderer; ///> Shared pointer to the renderer interface.
+        std::unique_ptr<Menu> _menu;        ///> Unique pointer to the menu.
+        StateManager *_manager = nullptr;   ///> Pointer to the state manager.
+    };
+}

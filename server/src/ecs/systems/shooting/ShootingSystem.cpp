7eaeca84c862c7d66ae5a1db50e2f7b6d2a8d7da
romain1717 <romain.berard@epitech.eu>
2025-12-23 22:19:48 +0100

    feat: refactor projectile creation in ShootingSystem for improved management

--- server/src/ecs/systems/shooting/ShootingSystem.cpp
+++ server/src/ecs/systems/shooting/ShootingSystem.cpp
@@ -6,7 +6,32 @@
 */
 
 #include "ShootingSystem.hpp"
-#include "Lifetime.hpp"
+
+struct ShootToCreate {
+    float x;
+    float y;
+    float vx;
+    float vy;
+    int damage;
+};
+
+namespace
+{
+    void createEntityProjectile(
+        Ecs::Registry &reg, const float x, const float y, const float vx, const float vy, const int damage)
+    {
+        const Ecs::Entity proj = reg.createEntity();
+        reg.emplaceComponent<Ecs::Position>(proj, Ecs::Position{x, y});
+        reg.emplaceComponent<Ecs::Velocity>(proj, Ecs::Velocity{vx, vy});
+        reg.emplaceComponent<Ecs::Damage>(proj, Ecs::Damage{damage});
+        reg.emplaceComponent<Ecs::Damageable>(proj);
+        reg.emplaceComponent<Ecs::Collision>(proj, Ecs::Collision{8.f, 8.f});
+        reg.emplaceComponent<Ecs::Drawable>(proj, Ecs::Drawable{6, true});
+        reg.emplaceComponent<Ecs::Health>(proj, Ecs::Health{1, 1});
+        reg.emplaceComponent<Ecs::Lifetime>(proj, Ecs::Lifetime{6.f});
+    }
+
+} // namespace
 
 namespace Game
 {
@@ -14,19 +39,15 @@ namespace Game
     {
         auto &reg = world.registry();
 
-        reg.view<InputComponent, Ecs::Position>([&reg](Ecs::Entity, InputComponent &input, const Ecs::Position &pos) {
+        std::vector<ShootToCreate> toCreate;
+
+        reg.view<InputComponent, Ecs::Position>([&](Ecs::Entity, InputComponent &input, const Ecs::Position &pos) {
             if (!input.shoot)
                 return;
             input.shoot = false;
-            const Ecs::Entity proj = reg.createEntity();
-            reg.emplaceComponent<Ecs::Position>(proj, pos.x + 30, pos.y);
-            reg.emplaceComponent<Ecs::Velocity>(proj, 100.f, 0.f);
-            reg.emplaceComponent<Ecs::Damage>(proj, 20);
-            reg.emplaceComponent<Ecs::Damageable>(proj);
-            reg.emplaceComponent<Ecs::Collision>(proj, 8.f, 8.f);
-            reg.emplaceComponent<Ecs::Drawable>(proj, static_cast<unsigned int>(6));
-            reg.emplaceComponent<Ecs::Health>(proj, Ecs::Health{1, 1});
-            reg.emplaceComponent<Ecs::Lifetime>(proj, 6.f);
+            toCreate.emplace_back(ShootToCreate{pos.x + 30, pos.y, 100.f, 0.f, 20});
         });
+        for (const auto &[x, y, vx, vy, damage] : toCreate)
+            createEntityProjectile(reg, x, y, vx, vy, damage);
     }
 } // namespace Game

2ea50df83b49c63ae5a7bbebb101749800f33d4d
romain1717 <romain.berard@epitech.eu>
2025-12-23 22:20:04 +0100

    feat: implement batch entity destruction in LifetimeSystem for improved efficiency

--- server/src/ecs/systems/lifetime/LifetimeSystem.cpp
+++ server/src/ecs/systems/lifetime/LifetimeSystem.cpp
@@ -14,10 +14,13 @@ namespace Game
     {
         auto &reg = world.registry();
 
+        std::vector<Ecs::Entity> toDestroy;
         reg.view<Ecs::Lifetime>([&](const Ecs::Entity e, Ecs::Lifetime &life) {
             life.remaining -= dt;
             if (life.remaining <= 0.f)
-                reg.destroyEntity(e);
+                toDestroy.emplace_back(e);
         });
+        for (const Ecs::Entity e : toDestroy)
+            reg.destroyEntity(e);
     }
 } // namespace Game

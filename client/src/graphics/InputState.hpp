327c3ea5eaecbf17f08c30188a2bf18a800bd9f0
ann7415 <anna.poghosyan@epitech.eu>
2025-12-24 04:22:53 +0100

    feat: add key press tracking and isPressed method to InputState

--- client/src/graphics/InputState.hpp
+++ client/src/graphics/InputState.hpp
@@ -6,6 +6,8 @@
 */
 
 #pragma once
+#include "Key.hpp"
+#include <unordered_set>
 
 namespace Engine
 {
@@ -16,15 +18,29 @@ namespace Engine
         float mouseX = 0.f; ///> Current mouse X position
         float mouseY = 0.f; ///> Current mouse Y position
 
+        std::unordered_set<Key> keysPressed; ///> Set of currently pressed keys
+
         bool mouseLeftDown = false;     ///> Is the left mouse button currently down
         bool mouseLeftPressed = false;  ///> Was the left mouse button pressed this frame
         bool mouseLeftReleased = false; ///> Was the left mouse button released this frame
 
         /**
-         * @brief Resets the per-frame input states.
+         * @brief Checks if a specific key is currently pressed.
+         *
+         * @param k The key to check.
+         * @return true if the key is pressed, false otherwise.
+         */
+        bool isPressed(const Key k) const
+        {
+            return keysPressed.contains(k);
+        }
+
+        /**
+         * @brief Resets the input state for the next frame.
          */
         void resetFrame()
         {
+            keysPressed.clear();
             mouseLeftPressed = false;
             mouseLeftReleased = false;
         }

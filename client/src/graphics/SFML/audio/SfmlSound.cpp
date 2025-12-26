4db2c71782c333a696bdce73a17f2584e0c65062
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-12-26 21:15:46 +0100

    feat: add SfmlSound class for managing SFML sound playback

---
+++ client/src/graphics/SFML/audio/SfmlSound.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** SfmlSound
+*/
+
+#include "SfmlSound.hpp"
+
+namespace Graphics
+{
+    SfmlSound::SfmlSound(std::shared_ptr<sf::SoundBuffer> buffer, float volume)
+        : _buffer(std::move(buffer)), _sound(*_buffer)
+    {
+        _sound.setVolume(volume);
+    }
+
+    void SfmlSound::play()
+    {
+        _sound.play();
+    }
+
+    void SfmlSound::stop()
+    {
+        _sound.stop();
+    }
+
+    bool SfmlSound::isPlaying() const
+    {
+        return _sound.getStatus() == sf::Sound::Status::Playing;
+    }
+
+    void SfmlSound::setVolume(float volume)
+    {
+        _sound.setVolume(volume);
+    }
+} // namespace Graphics
---
+++ client/src/graphics/SFML/audio/SfmlSound.hpp
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** SfmlSound
+*/
+
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <memory>
+
+namespace Graphics
+{
+    /**
+     * @class SfmlSound
+     * @brief Wrapper class for managing an active SFML sound.
+     * This class handles the lifecycle of a playing sound effect.
+     */
+    class SfmlSound {
+      public:
+        /**
+         * @brief Constructor for SfmlSound.
+         * @param buffer Shared pointer to the sound buffer.
+         * @param volume Volume level (0.0 to 100.0).
+         */
+        explicit SfmlSound(std::shared_ptr<sf::SoundBuffer> buffer, float volume = 100.f);
+
+        /**
+         * @brief Play the sound.
+         */
+        void play();
+
+        /**
+         * @brief Stop the sound.
+         */
+        void stop();
+
+        /**
+         * @brief Check if the sound is currently playing.
+         * @return True if playing, false otherwise.
+         */
+        [[nodiscard]] bool isPlaying() const;
+
+        /**
+         * @brief Set the volume of the sound.
+         * @param volume Volume level (0.0 to 100.0).
+         */
+        void setVolume(float volume);
+
+      private:
+        std::shared_ptr<sf::SoundBuffer> _buffer; ///> Sound buffer
+        sf::Sound _sound; ///> SFML sound object
+    };
+} // namespace Graphics

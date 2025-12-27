1e7dbe8836c30d25255913b070278b1bc6d04f21
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-12-28 00:01:32 +0100

    feat: add SfmlMusic class for managing SFML music playback

---
+++ client/src/graphics/SFML/audio/SfmlMusic.cpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** SfmlMusic
+*/
+
+#include "SfmlMusic.hpp"
+
+namespace Graphics
+{
+    SfmlMusic::SfmlMusic(std::unique_ptr<sf::Music> music)
+        : _music(std::move(music))
+    {
+    }
+
+    void SfmlMusic::play()
+    {
+        if (_music)
+            _music->play();
+    }
+
+    void SfmlMusic::stop()
+    {
+        if (_music)
+            _music->stop();
+    }
+
+    void SfmlMusic::pause()
+    {
+        if (_music)
+            _music->pause();
+    }
+
+    bool SfmlMusic::isPlaying() const
+    {
+        return _music && _music->getStatus() == sf::Music::Status::Playing;
+    }
+
+    void SfmlMusic::setVolume(float volume)
+    {
+        if (_music)
+            _music->setVolume(volume);
+    }
+
+    void SfmlMusic::setLooping(bool loop)
+    {
+        if (_music)
+            _music->setLooping(loop);
+    }
+
+    sf::Music* SfmlMusic::get() noexcept
+    {
+        return _music.get();
+    }
+} // namespace Graphics
---
+++ client/src/graphics/SFML/audio/SfmlMusic.hpp
@@ -0,0 +1,70 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** SfmlMusic
+*/
+
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <memory>
+
+namespace Graphics
+{
+    /**
+     * @class SfmlMusic
+     * @brief Wrapper class for managing SFML music.
+     * This class handles the lifecycle of a music track.
+     */
+    class SfmlMusic {
+      public:
+        /**
+         * @brief Constructor for SfmlMusic.
+         * @param music Unique pointer to the music object.
+         */
+        explicit SfmlMusic(std::unique_ptr<sf::Music> music);
+
+        /**
+         * @brief Play the music.
+         */
+        void play();
+
+        /**
+         * @brief Stop the music.
+         */
+        void stop();
+
+        /**
+         * @brief Pause the music.
+         */
+        void pause();
+
+        /**
+         * @brief Check if the music is currently playing.
+         * @return True if playing, false otherwise.
+         */
+        [[nodiscard]] bool isPlaying() const;
+
+        /**
+         * @brief Set the volume of the music.
+         * @param volume Volume level (0.0 to 100.0).
+         */
+        void setVolume(float volume);
+
+        /**
+         * @brief Set whether the music should loop.
+         * @param loop True to loop, false otherwise.
+         */
+        void setLooping(bool loop);
+
+        /**
+         * @brief Get the underlying SFML music object.
+         * @return Pointer to the sf::Music object.
+         */
+        [[nodiscard]] sf::Music* get() noexcept;
+
+      private:
+        std::unique_ptr<sf::Music> _music; ///> SFML music object
+    };
+} // namespace Graphics

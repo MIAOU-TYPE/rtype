44290c24e2ec54e1dcacb0195e6f3d8e1204bf0c
romain1717 <romain.berard@epitech.eu>
2025-12-31 00:15:20 +0100

    feat: make setNonBlocking pure virtual in AServer to enforce implementation in derived classes

--- server/src/server/interfaces/AServer.hpp
+++ server/src/server/interfaces/AServer.hpp
@@ -59,7 +59,7 @@ namespace Net::Server
          * @brief Sets the server's socket to non-blocking or blocking mode.
          * @param nonBlocking True to set the socket to non-blocking mode, false for blocking mode.
          */
-        void setNonBlocking(bool nonBlocking) override;
+        void setNonBlocking(bool nonBlocking) override = 0;
 
         /**
          * @brief Checks if the server is currently running.

60d7dff30f6ec625dde485ffeb0f64dc7b0fb064
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-12-31 15:33:50 +0100

    feat: initialize ColorBlindManager with resource manager in SfmlRenderer

--- client/src/graphics/SFML/renderer/SfmlRenderer.cpp
+++ client/src/graphics/SFML/renderer/SfmlRenderer.cpp
@@ -20,7 +20,7 @@ namespace Graphics
         _fontManager = std::make_shared<SfmlFontManager>(_resourceManager);
         _textureManager = std::make_shared<SfmlTextureManager>(_resourceManager);
         _textManager = std::make_shared<SfmlTextManager>(_fontManager);
-        _colorBlindManager = std::make_unique<ColorBlindManager>();
+        _colorBlindManager = std::make_unique<ColorBlindManager>(_resourceManager);
         _renderTexture = std::make_unique<sf::RenderTexture>();
         const auto windowSize = _window->getSize();
         if (!_renderTexture->resize(windowSize)) {
@@ -37,7 +37,6 @@ namespace Graphics
 
     void SfmlRenderer::beginFrame()
     {
-        // Update render texture size if window size changed
         if (_renderTexture && _colorBlindManager->isShaderAvailable()) {
             const auto windowSize = _window->getSize();
             const auto textureSize = _renderTexture->getSize();

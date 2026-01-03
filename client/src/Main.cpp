00486ac5d7adefef360ef2e78a5c386f2e940970
romain1717 <romain.berard@epitech.eu>
2026-01-03 16:43:14 +0100

    feat: implement TCP client alongside UDP client for session management

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -11,6 +11,8 @@
 #include "EventBus.hpp"
 #include "EventRegistry.hpp"
 #include "SfmlGraphics.hpp"
+#include "TCPClient.hpp"
+#include "TCPPacket.hpp"
 #include "UDPClient.hpp"
 
 namespace
@@ -48,20 +50,26 @@ int main(const int argc, char **argv)
             return result == Utils::ArgParseResult::HelpDisplayed ? 0 : 84;
 
         const auto graphics = std::make_shared<Graphics::SfmlGraphics>();
-        const auto client = std::make_shared<Network::UDPClient>();
+        const auto udpClient = std::make_shared<Network::UDPClient>();
+        const auto tcpClient = std::make_shared<Network::TCPClient>();
 
-        Thread::ClientRuntime clientRuntime(graphics, client);
+        Thread::ClientRuntime clientRuntime(graphics, udpClient);
         const auto eventBus = clientRuntime.getEventBus();
         Engine::EventRegistry eventRegistry(eventBus);
 
         createEventHandler(eventRegistry, clientRuntime, eventBus);
         std::cout << "Connecting to " << argParser.getHost() << ":" << argParser.getPort() << "..." << std::endl;
-        client->configure(argParser.getHost(), argParser.getPort());
-
+        tcpClient->configure(argParser.getHost(), argParser.getPort());
+        udpClient->configure(argParser.getHost(), argParser.getPort() + 1);
         clientRuntime.start();
-        clientRuntime.runDisplay();
+
+        tcpClient->start();
+        // clientRuntime.runDisplay();
         clientRuntime.wait();
 
+        while (tcpClient->isRunning()) {
+            tcpClient->receivePackets();
+        }
         return 0;
     } catch (std::exception &e) {
         std::cerr << "{Main}: " << e.what() << std::endl;

77e1f08e20b952c8d76920a2c8eb2989746cb2b8
SantiagoPidji <santiago.pidcova@epitech.eu>
2026-01-05 12:35:26 +0100

    fix: adjust formatting of sound settings variables in SettingsMenu

--- client/src/ui/settings/SettingsMenu.hpp
+++ client/src/ui/settings/SettingsMenu.hpp
@@ -126,10 +126,10 @@ namespace Engine
         std::unique_ptr<UI::UIButton> _resolution;     ///> Current resolution display button
         std::unique_ptr<UI::UIButton> _resolutionNext; ///> Next resolution button
 
-        std::size_t _musicVolume = 50;    ///> Music volume level (0-100)
-        std::size_t _sfxVolume = 50;      ///> SFX volume level (0-100)
-        bool _musicMuted = false; ///> Music mute state
-        bool _sfxMuted = false;   ///> SFX mute state
+        std::size_t _musicVolume = 50; ///> Music volume level (0-100)
+        std::size_t _sfxVolume = 50;   ///> SFX volume level (0-100)
+        bool _musicMuted = false;      ///> Music mute state
+        bool _sfxMuted = false;        ///> SFX mute state
 
         std::unique_ptr<UI::UIButton> _musicVolLabel; ///> Music volume display
         std::unique_ptr<UI::UIButton> _musicVolUp;    ///> Increase music volume

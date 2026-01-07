2771d4a574ae0440c796f0e01c30acf6c4acc7c6
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-07 15:20:57 +0100

    feat: enable state manager updates and rendering in the main loop

--- client/src/thread/ClientRuntime.cpp
+++ client/src/thread/ClientRuntime.cpp
@@ -122,7 +122,7 @@ namespace Thread
 
             _graphics->pollEvents(*_eventBus);
             _eventBus->dispatch();
-            // _stateManager->update(_input->consumeFrame());
+            _stateManager->update(_input->consumeFrame());
 
             {
                 std::scoped_lock lock(_frameMutex);
@@ -133,7 +133,7 @@ namespace Thread
                 for (const auto &cmd : *localRenderCommands)
                     _renderer->draw(cmd);
             }
-            // _stateManager->render();
+            _stateManager->render();
             _renderer->endFrame();
             syncToNextTick(nextTick, Tick * 2);
         }

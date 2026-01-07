0dd6276ae80ccd21333c920d098472c4ad0f9149
romain1717 <romain.berard@epitech.eu>
2026-01-07 03:44:09 +0100

    feat: add EventBus support to RoomState for room management actions

--- client/src/engine/state/room/RoomState.cpp
+++ client/src/engine/state/room/RoomState.cpp
@@ -11,9 +11,9 @@ namespace Engine
 {
     RoomState::RoomState(std::shared_ptr<Graphics::IGraphics> graphics, std::shared_ptr<Graphics::IRenderer> renderer,
         std::shared_ptr<MusicRegistry> musicRegistry, std::shared_ptr<SoundRegistry> soundRegistry,
-        std::shared_ptr<RoomManager> roomManager)
+        std::shared_ptr<RoomManager> roomManager, std::shared_ptr<EventBus> eventBus)
         : _graphics(std::move(graphics)), _renderer(std::move(renderer)), _musicRegistry(std::move(musicRegistry)),
-          _soundRegistry(std::move(soundRegistry)), _roomManager(std::move(roomManager))
+          _soundRegistry(std::move(soundRegistry)), _roomManager(std::move(roomManager)), _eventBus(std::move(eventBus))
     {
     }
 
@@ -27,8 +27,16 @@ namespace Engine
     {
         _menu->update(frame);
         if (_menu->wantsBackToMenu())
-            manager.queueState(
-                std::make_unique<MenuState>(_graphics, _renderer, _musicRegistry, _soundRegistry, _roomManager));
+            manager.queueState(std::make_unique<MenuState>(
+                _graphics, _renderer, _musicRegistry, _soundRegistry, _roomManager, _eventBus));
+        if (auto &create = _menu->wantsCreateRoom()) {
+            create = false;
+            _eventBus->emit<CreateRoomRequested>(CreateRoomRequested("default", 2));
+        }
+        if (auto &join = _menu->wantsJoinRoom()) {
+            join = false;
+            _eventBus->emit<JoinRoomRequested>(JoinRoomRequested(1));
+        }
     }
 
     void RoomState::render()

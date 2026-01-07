bacc880be712e996fa0abbf36ffa5f66916b59b2
romain1717 <romain.berard@epitech.eu>
2026-01-07 03:42:52 +0100

    feat: implement GameState class for managing game state transitions

---
+++ client/src/engine/state/game/GameState.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** GameState
+*/
+
+#pragma once
+
+#include "IGameState.hpp"
+#include "IGraphics.hpp"
+#include "IRenderer.hpp"
+#include "Menu.hpp"
+#include "MusicRegistry.hpp"
+#include "SoundRegistry.hpp"
+
+namespace Engine
+{
+    /**
+     * @brief Represents the Game state of the game.
+     */
+    class GameState : public IGameState {
+      public:
+        /**
+         * @brief Construct a new Game State object.
+         * @param graphics Shared pointer to the graphics interface.
+         * @param renderer Shared pointer to the renderer interface.
+         * @param musicRegistry Shared pointer to the music registry.
+         * @param soundRegistry Shared pointer to the sound registry.
+         */
+        explicit GameState(std::shared_ptr<Graphics::IGraphics> graphics, std::shared_ptr<Graphics::IRenderer> renderer,
+            std::shared_ptr<MusicRegistry> musicRegistry, std::shared_ptr<SoundRegistry> soundRegistry);
+
+        /**
+         * @brief Called when entering the state.
+         */
+        void onEnter() override;
+
+        /**
+         * @brief Update the Game state.
+         * @param manager Reference to the state manager.
+         * @param frame The current input frame.
+         */
+        void update(StateManager &manager, const InputFrame &frame) override;
+
+        /**
+         * @brief Render the Game state.
+         */
+        void render() override;
+
+      private:
+        std::shared_ptr<Graphics::IGraphics> _graphics; ///> Shared pointer to the graphics interface.
+        std::shared_ptr<Graphics::IRenderer> _renderer; ///> Shared pointer to the renderer interface.
+        std::shared_ptr<MusicRegistry> _musicRegistry;  ///> Shared pointer to the music registry.
+        std::shared_ptr<SoundRegistry> _soundRegistry;  ///> Shared pointer to the sound registry.
+    };
+} // namespace Engine

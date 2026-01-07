ce7cd9d74a6570341106d8e8b5d42f58347dbb02
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-07 16:36:40 +0100

    feat: mark applyMusicVolumeChange and applySoundVolumeChange as noexcept

--- client/src/ui/settings/SettingsMenu.hpp
+++ client/src/ui/settings/SettingsMenu.hpp
@@ -116,14 +116,14 @@ namespace Engine
          * @param volume The new volume value.
          * @param isMuted Whether the audio is currently muted.
          */
-        [[noexcept]] void applyMusicVolumeChange(size_t volume, bool isMuted);
+        void applyMusicVolumeChange(size_t volume, bool isMuted) noexcept;
 
         /**
          * @brief Apply volume change to sound registry, handling mute state.
          * @param volume The new volume value.
          * @param isMuted Whether the audio is currently muted.
          */
-        [[noexcept]] void applySoundVolumeChange(size_t volume, bool isMuted);
+        void applySoundVolumeChange(size_t volume, bool isMuted) noexcept;
 
         std::shared_ptr<Graphics::IRenderer> _renderer; ///> Renderer used for rendering the settings menu
         std::shared_ptr<MusicRegistry> _musicRegistry;  ///> Music registry

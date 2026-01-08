e5e95494f2d52e96947f75feef3fd7e243bf6ecf
romain1717 <romain.berard@epitech.eu>
2026-01-08 21:09:56 +0100

    fix: remove redundant std:: prefix for uint8_t in HeaderData

--- shared/Network/Data/UDP/HeaderData.hpp
+++ shared/Network/Data/UDP/HeaderData.hpp
@@ -8,7 +8,7 @@
 #pragma once
 #include <cstdint>
 
-inline constexpr std::uint8_t kPacketMagic[4] = {'R', 'T', 'Y', 'P'};
+inline constexpr uint8_t kPacketMagic[4] = {'R', 'T', 'Y', 'P'};
 
 #pragma pack(push, 1)
 
@@ -17,10 +17,10 @@ inline constexpr std::uint8_t kPacketMagic[4] = {'R', 'T', 'Y', 'P'};
  * @details This structure contains the type, version, and size of the packet.
  */
 struct HeaderData {
-    std::uint8_t magic[4]; ///> Magic number to identify the packet (should be 'RTYP').
-    uint8_t type;          ///> The type of the packet (e.g., CONNECT, DISCONNECT, INPUT, etc.).
-    uint8_t version;       ///> The version of the packet format.
-    uint16_t size;         ///> The size of the entire packet, including the header.
+    uint8_t magic[4]; ///> Magic number to identify the packet (should be 'RTYP').
+    uint8_t type;     ///> The type of the packet (e.g., CONNECT, DISCONNECT, INPUT, etc.).
+    uint8_t version;  ///> The version of the packet format.
+    uint16_t size;    ///> The size of the entire packet, including the header.
 };
 
 #pragma pack(pop)

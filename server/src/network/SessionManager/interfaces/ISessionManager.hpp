f36b9a8bc5df6390f808df3475091186d326ead6
romain1717 <romain.berard@epitech.eu>
2026-01-09 02:46:57 +0100

    feat: add sequence validation method to ISessionManager for UDP sessions

--- server/src/network/SessionManager/interfaces/ISessionManager.hpp
+++ server/src/network/SessionManager/interfaces/ISessionManager.hpp
@@ -109,5 +109,12 @@ namespace Net::Server
          * @return The session ID, or -1 if not found.
          */
         [[nodiscard]] virtual int getSessionIdFromUdp(const sockaddr_in &udpAddr) const = 0;
+
+        /**
+         * @brief Check if the given sequence number is valid for the session.
+         * @param sessionId The ID of the session.
+         * @param sequence The sequence number to set.
+         */
+        [[nodiscard]] virtual bool isSequenceValid(int sessionId, uint32_t sequence) const noexcept = 0;
     };
 } // namespace Net::Server

527defeb68dbe84c398cb0318d8295b27ef46901
romain1717 <romain.berard@epitech.eu>
2026-01-09 01:48:26 +0100

    feat: modify onSnapshot method to include sequence number for snapshot data

--- client/src/game/controller/ClientController.cpp
+++ client/src/game/controller/ClientController.cpp
@@ -33,9 +33,9 @@ namespace Ecs
         std::cout << "onGameOver" << std::endl;
     }
 
-    void ClientController::onSnapshot(const std::vector<SnapshotEntity> &data)
+    void ClientController::onSnapshot(const std::vector<SnapshotEntity> &data, const uint32_t sequence)
     {
-        _commandBuffer.get().push({World::WorldCommand::Type::Snapshot, data});
+        _commandBuffer.get().push({World::WorldCommand::Type::Snapshot, data, sequence});
     }
 
     void ClientController::onScore(const uint32_t score)

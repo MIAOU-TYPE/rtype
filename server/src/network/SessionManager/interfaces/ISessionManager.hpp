e10ac581aba16c423c7873749109430a6c0bd04c
romain1717 <romain.berard@epitech.eu>
2026-01-09 03:14:17 +0100

    feat: update sequence number validation description in ISessionManager

--- server/src/network/SessionManager/interfaces/ISessionManager.hpp
+++ server/src/network/SessionManager/interfaces/ISessionManager.hpp
@@ -111,7 +111,8 @@ namespace Net::Server
         [[nodiscard]] virtual int getSessionIdFromUdp(const sockaddr_in &udpAddr) const = 0;
 
         /**
-         * @brief Check if the given sequence number is valid for the session.
+         * @brief Check if the given sequence number is valid for the session and, if so, update the last seen sequence
+         * number.
          * @param sessionId The ID of the session.
          * @param sequence The sequence number to set.
          */

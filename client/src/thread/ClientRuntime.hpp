bf74e05b788e54a88c525f50d589a185201f63f9
romain1717 <romain.berard@epitech.eu>
2026-01-09 19:01:17 +0100

    feat: change _readRenderCommands to a non-const shared pointer for mutable access

--- client/src/thread/ClientRuntime.hpp
+++ client/src/thread/ClientRuntime.hpp
@@ -166,7 +166,7 @@ namespace Thread
         Command::CommandBuffer<World::WorldCommand> _commandBuffer; ///> Command buffer for storing commands
 
         std::mutex _frameMutex;
-        std::shared_ptr<const std::vector<Engine::RenderCommand>> _readRenderCommands;
+        std::shared_ptr<std::vector<Engine::RenderCommand>> _readRenderCommands;
         std::shared_ptr<std::vector<Engine::RenderCommand>> _writeRenderCommands;
 
         std::thread _receiverThread; ///> Thread for receiving packets

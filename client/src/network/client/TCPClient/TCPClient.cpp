0b5348504e6f3c1ce3c774a5aaa7a511e626ed7a
romain1717 <romain.berard@epitech.eu>
2026-01-11 00:27:03 +0100

    feat: add startup log for TCP server initialization in TCPClient

--- client/src/network/client/TCPClient/TCPClient.cpp
+++ client/src/network/client/TCPClient/TCPClient.cpp
@@ -77,6 +77,7 @@ namespace Network
             close();
             throw;
         }
+        std::cout << "{TCPClient::start} TCP Server started on " << _ip << ":" << _port << std::endl;
     }
 
     void TCPClient::close()

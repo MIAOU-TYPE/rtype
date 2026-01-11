4c29a98772ef15066e461698d81100bb4a736ba3
ann7415 <anna.poghosyan@epitech.eu>
2026-01-11 01:26:09 +0100

    refactor: adjust spacing in TCPMessageSink for improved readability

--- client/src/network/packet/router/TCP/sink/TCPHandler/TCPMessageSink.hpp
+++ client/src/network/packet/router/TCP/sink/TCPHandler/TCPMessageSink.hpp
@@ -189,7 +189,7 @@ namespace Network
         std::vector<RoomIdCb> _roomLeftCbs;        ///> Callbacks for room left events
         std::vector<RoomIdCb> _gameStartCbs;       ///> Callbacks for game start events
         std::vector<ProtoErrCb> _protocolErrorCbs; ///> Callbacks for protocol error events
-        std::vector<AuthOkCb> _authOkCbs;   ///> Callbacks for authentication success events
+        std::vector<AuthOkCb> _authOkCbs;          ///> Callbacks for authentication success events
 
         bool _isConnected = false;    ///> Connection status
         ConnectInfo _connectData{};   ///> Connection information
@@ -199,8 +199,8 @@ namespace Network
          * @brief Structure representing the identity of the authenticated user.
          */
         struct Identity {
-            uint32_t userId = 0; ///> User ID
-            std::string username;   ///> Username
+            uint32_t userId = 0;  ///> User ID
+            std::string username; ///> Username
         };
 
         std::optional<Identity> _identity{}; ///> Authenticated user identity

0c6ab02df7be8ace2a8ea9c902ba8711b79c3527
romain1717 <romain.berard@epitech.eu>
2026-01-11 06:43:56 +0100

    style: format UDPTypesData.hpp for improved readability and consistency

--- shared/Network/Data/UDP/UDPTypesData.hpp
+++ shared/Network/Data/UDP/UDPTypesData.hpp
@@ -37,13 +37,13 @@ namespace Net::Protocol::UDP
     /**
      * @brief Packet from server to client.
      */
-    constexpr uint8_t ACCEPT = 0x10;       ///> Server accepts the client's connection request
-    constexpr uint8_t REJECT = 0x11;       ///> Server rejects the client's connection request
-    constexpr uint8_t SNAPSHOT = 0x12;     ///> Server sends a game state snapshot to the client
-    constexpr uint8_t PONG = 0x13;         ///> Server responds to client's ping
-    constexpr uint8_t DAMAGE_EVENT = 0x14; ///> Server notifies client of a damage event
-    constexpr uint8_t GAME_OVER = 0x15;    ///> Server notifies client of game over event
-    constexpr uint8_t SCORE = 0x16;        ///> Server sends score update to the client
+    constexpr uint8_t ACCEPT = 0x10;         ///> Server accepts the client's connection request
+    constexpr uint8_t REJECT = 0x11;         ///> Server rejects the client's connection request
+    constexpr uint8_t SNAPSHOT = 0x12;       ///> Server sends a game state snapshot to the client
+    constexpr uint8_t PONG = 0x13;           ///> Server responds to client's ping
+    constexpr uint8_t DAMAGE_EVENT = 0x14;   ///> Server notifies client of a damage event
+    constexpr uint8_t GAME_OVER = 0x15;      ///> Server notifies client of game over event
+    constexpr uint8_t SCORE = 0x16;          ///> Server sends score update to the client
     constexpr uint8_t DESTROY_ENTITY = 0x17; ///> Server notifies client to destroy an entity
 
 } // namespace Net::Protocol::UDP

d9a18d889b944b91ef9b3b026f4164276e850a7f
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-14 20:56:07 +0100

    feat: integrate BossSystem updates into GameServer for enhanced boss behavior

--- server/src/game/gameServer/GameServer.cpp
+++ server/src/game/gameServer/GameServer.cpp
@@ -113,6 +113,7 @@ namespace Game
 
         BackgroundSystem::update(*_worldWrite, dt);
         BackgroundSystem::resetScroll(*_worldWrite);
+        BossSystem::update(*_worldWrite, dt);
         AIShootSystem::update(*_worldWrite, dt);
 
         InputSystem::update(*_worldWrite);

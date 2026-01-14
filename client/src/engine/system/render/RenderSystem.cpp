d79d68e772abab0fb884af9afd88290e58e0cf6f
EvannBloutin <evann.bloutin@epitech.eu>
2026-01-14 16:44:49 +0100

    fix: adjust finalY calculation for spriteId 16 to include position offset

--- client/src/engine/system/render/RenderSystem.cpp
+++ client/src/engine/system/render/RenderSystem.cpp
@@ -37,7 +37,7 @@ namespace Engine
                 if (drawable.spriteId == 16) {
                     const float viewportHeight = static_cast<float>(viewportSize.height);
                     finalX = 10.f;
-                    finalY = viewportHeight - 50.f;
+                    finalY = viewportHeight - 50.f - pos.y;
                     cmd.position = {finalX, finalY};
                     cmd.scale = {2.f, 2.f};
                 }

71d0e054bd5c7d49825a7986428bcb1cfd940a40
SantiagoPidji <santiago.pidcova@epitech.eu>
2026-01-14 17:21:59 +0100

    fix: update key bindings to use settings from SettingsConfig

--- client/src/thread/ClientRuntime.cpp
+++ client/src/thread/ClientRuntime.cpp
@@ -264,19 +264,19 @@ namespace Thread
     {
         const auto keys = Utils::SettingsConfig::getInstance().getMovementKeys();
 
-        _eventRegistry->onKeyPressed(Engine::Key::Up, [this]() {
+        _eventRegistry->onKeyPressed(keys.up, [this]() {
             _udpClient->sendPacket(*_udpPacketFactory.makeInput(PlayerInput{true, false, false, false, false}));
         });
 
-        _eventRegistry->onKeyPressed(Engine::Key::Down, [this]() {
+        _eventRegistry->onKeyPressed(keys.down, [this]() {
             _udpClient->sendPacket(*_udpPacketFactory.makeInput(PlayerInput{false, true, false, false, false}));
         });
 
-        _eventRegistry->onKeyPressed(Engine::Key::Left, [this]() {
+        _eventRegistry->onKeyPressed(keys.left, [this]() {
             _udpClient->sendPacket(*_udpPacketFactory.makeInput(PlayerInput{false, false, true, false, false}));
         });
 
-        _eventRegistry->onKeyPressed(Engine::Key::Right, [this]() {
+        _eventRegistry->onKeyPressed(keys.right, [this]() {
             _udpClient->sendPacket(*_udpPacketFactory.makeInput(PlayerInput{false, false, false, true, false}));
         });
 

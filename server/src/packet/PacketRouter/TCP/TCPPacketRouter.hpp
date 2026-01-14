93c4872abc54f84c30d8c44fe87373dd7d825b77
ann7415 <anna.poghosyan@epitech.eu>
2026-01-14 21:27:08 +0100

    feat: add SCOREBOARD_GET and SCORE_SUBMIT handlers to TCPPacketRouter

--- server/src/packet/PacketRouter/TCP/TCPPacketRouter.hpp
+++ server/src/packet/PacketRouter/TCP/TCPPacketRouter.hpp
@@ -11,6 +11,7 @@
 #include "RoomManager.hpp"
 #include "TCPPacketFactory.hpp"
 #include "TCPTypesData.hpp"
+#include "ScoreService.hpp"
 
 namespace Net
 {
@@ -27,10 +28,11 @@ namespace Net
          * @param tcpServer The TCP server
          * @param packetFactory The TCP packet factory for outgoing packets
          * @param authService The authentication service
+         * @param scoreService The score service
          */
         TCPPacketRouter(std::shared_ptr<Server::ISessionManager> sessions, std::shared_ptr<Engine::RoomManager> rooms,
             std::shared_ptr<Server::IServer> tcpServer, std::shared_ptr<Factory::TCPPacketFactory> packetFactory,
-            std::shared_ptr<Auth::AuthService> authService);
+            std::shared_ptr<Auth::AuthService> authService, std::shared_ptr<Engine::ScoreService> scoreService);
 
         /**
          * @brief Handles an incoming TCP packet
@@ -115,11 +117,29 @@ namespace Net
          */
         void onAuthLogin(const sockaddr_in &addr, int sessionId, uint32_t req, TCP::Reader &r) const;
 
+        /**
+         * @brief Handles the SCORE_SUBMIT packet from a client
+         * @param addr The address of the client
+         * @param sessionId The session ID of the client
+         * @param req The request ID
+         * @param r The TCP reader for the packet body
+         */
+        void onScoreSubmit(const sockaddr_in &addr, int sessionId, uint32_t req, TCP::Reader &r) const;
+
+        /**
+         * @brief Handles the SCOREBOARD_GET packet from a client
+         * @param addr The address of the client
+         * @param req The request ID
+         * @param r The TCP reader for the packet body
+         */
+        void onScoreboardGet(const sockaddr_in &addr, uint32_t req, TCP::Reader &r) const;
+
         std::shared_ptr<Server::ISessionManager> _sessions = nullptr;        ///> Session manager
         std::shared_ptr<Engine::RoomManager> _rooms = nullptr;               ///> Room manager
         std::shared_ptr<Server::IServer> _tcp = nullptr;                     ///> TCP server
         std::shared_ptr<Factory::TCPPacketFactory> _packetFactory = nullptr; ///> TCP packet factory
         std::shared_ptr<Auth::AuthService> _auth;                            ///> Authentication service
+        std::shared_ptr<Engine::ScoreService> _scores;      ///> Score service
 
         int32_t _serverUdpPort = 0; ///> UDP port to send to clients
     };

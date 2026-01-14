c360d6fdcf81424075b4df191d7e4c9b09ee0632
ann7415 <anna.poghosyan@epitech.eu>
2026-01-14 20:30:12 +0100

    feat: add Score command type for score updates in WorldCommand

---
+++ shared/Network/Data/TCP/ScoreEntry.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2025
+** ClientRuntime.cpp
+** File description:
+** ScoreEntry
+*/
+
+#pragma once
+#include <string>
+
+/**
+ * @struct ScoreEntry
+ * @brief Structure representing a user's score entry.
+ */
+struct ScoreEntry {
+    std::string username;       ///> The username of the player
+    int score;  ///> The score achieved by the player
+};

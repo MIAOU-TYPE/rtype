fd94b5d3c2f733317a84c0c460230409c6148983
romain1717 <romain.berard@epitech.eu>
2026-01-14 23:05:11 +0100

    feat: remove Attack struct definition from Attack.hpp

--- server/src/thread/ServerRuntime.cpp
+++ server/src/thread/ServerRuntime.cpp
@@ -20,10 +20,10 @@ ServerRuntime::ServerRuntime(
 
     try {
         std::error_code ec;
-        std::filesystem::create_directories("data", ec);
+        std::filesystem::create_directories("db", ec);
         if (ec)
             throw ThreadError(std::string("{ServerRuntime} create_directories failed: ") + ec.message());
-        _authDb = std::make_shared<Auth::SqliteDb>("data/users.sqlite3");
+        _authDb = std::make_shared<Auth::SqliteDb>("db/users.sqlite3");
         _userRepo = std::make_shared<Auth::UserStorage>(_authDb);
         _userRepo->initSchema();
         _authService = std::make_shared<Auth::AuthService>(_userRepo);

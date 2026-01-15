e07d25dbc9ec2059ad3f15c9ac112b77d6c08d07
SantiagoPidji <santiago.pidcova@epitech.eu>
2026-01-15 12:01:20 +0100

    fix: update shooting cooldown management to use a member variable for last shoot time

--- client/src/thread/ClientRuntime.cpp
+++ client/src/thread/ClientRuntime.cpp
@@ -265,14 +265,13 @@ namespace Thread
         if (_input->isKeyHeld(right))
             input.right = true;
 
-        static auto lastShootTime = clock::now();
         constexpr auto ShootCooldown = std::chrono::milliseconds(250);
 
         if (_input->isKeyHeld(shoot)) {
             const auto now = clock::now();
-            if (now - lastShootTime >= ShootCooldown) {
+            if (now - _lastShootTime >= ShootCooldown) {
                 input.shoot = true;
-                lastShootTime = now;
+                _lastShootTime = now;
             }
         }
 

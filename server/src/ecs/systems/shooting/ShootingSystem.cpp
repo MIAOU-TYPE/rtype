3f2386cb8173a383f1b8cdbaec4d52f5c92f0e7b
SantiagoPidji <santiago.pidcova@epitech.eu>
2026-01-15 16:05:06 +0100

    feat: enhance shooting system with cooldown management

--- server/src/ecs/systems/shooting/ShootingSystem.cpp
+++ server/src/ecs/systems/shooting/ShootingSystem.cpp
@@ -13,14 +13,31 @@ namespace Game
     {
         auto &reg = world.registry();
 
-        reg.view<InputComponent, Ecs::Position, Ecs::WeaponConfig>(
+        reg.view<InputComponent, Ecs::Position, Ecs::WeaponConfig, Ecs::ShootCooldown>(
             [&](const Ecs::Entity entity, InputComponent &input, const Ecs::Position &pos,
-                const Ecs::WeaponConfig &weapon) {
+                const Ecs::WeaponConfig &weapon, Ecs::ShootCooldown &cooldown) {
                 if (!input.shoot)
                     return;
+
+                if (cooldown.timer > 0.f) {
+                    input.shoot = false;
+                    return;
+                }
+
                 input.shoot = false;
+                cooldown.timer = cooldown.cooldown;
                 world.events().emit<ShootEvent>(ShootEvent{pos.x + 40, pos.y + 7.5f, 100.f, 0.f, 20,
                     static_cast<size_t>(entity), {8.f, 8.f}, 5.f, weapon.projectileSpriteId});
             });
+    }
+
+    void ShootingSystem::updateCooldowns(IGameWorld &world, const float dt)
+    {
+        auto &reg = world.registry();
+
+        reg.view<Ecs::ShootCooldown>([&](const Ecs::Entity, Ecs::ShootCooldown &cooldown) {
+            if (cooldown.timer > 0.f)
+                cooldown.timer -= dt;
+        });
     }
 } // namespace Game
--- server/src/ecs/systems/shooting/ShootingSystem.hpp
+++ server/src/ecs/systems/shooting/ShootingSystem.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include "ShootCooldown.hpp"
 #include "WeaponConfig.hpp"
 #include "World.hpp"
 
@@ -21,5 +22,11 @@ namespace Game
          * @param world The game world containing all entities and components.
          */
         static void update(IGameWorld &world);
+
+        /** @brief Update shoot cooldown timers.
+         * @param world The game world containing all entities and components.
+         * @param dt Delta time in seconds.
+         */
+        static void updateCooldowns(IGameWorld &world, float dt);
     };
 } // namespace Game

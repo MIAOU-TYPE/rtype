d488a5ecc4918e3e2a83cd315f917d3836ad4086
SantiagoPidji <santiago.pidcova@epitech.eu>
2026-01-15 11:39:31 +0100

    fix: update InputSystem to use game configuration constants for speed and viewport dimensions

--- server/src/ecs/systems/input/InputSystem.cpp
+++ server/src/ecs/systems/input/InputSystem.cpp
@@ -13,11 +13,11 @@ namespace Game
     {
         world.registry().view<InputComponent, Ecs::Position, Ecs::Collision>(
             [](Ecs::Entity, InputComponent &input, Ecs::Position &pos, const Ecs::Collision &col) {
-                constexpr float speed = 3.5f;
+                constexpr float speed = Game::Config::PLAYER_SPEED;
                 constexpr float minX = 0.f;
                 constexpr float minY = 0.f;
-                const float maxX = 1280.f - col.width;
-                const float maxY = 720.f - col.height;
+                const float maxX = Game::Config::VIEWPORT_WIDTH - col.width;
+                const float maxY = Game::Config::VIEWPORT_HEIGHT - col.height;
                 float dx = 0.f, dy = 0.f;
                 constexpr float inv_sqrt2 = 0.7071067811865476f;
 
--- server/src/ecs/systems/input/InputSystem.hpp
+++ server/src/ecs/systems/input/InputSystem.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "World.hpp"
+#include "GameConfig.hpp"
 
 namespace Game
 {

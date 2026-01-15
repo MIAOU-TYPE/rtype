a5380b4226e97ddc250a0bb5ddab10b5dbb31b1b
EvannBloutin <evann.bloutin@epitech.eu>
2026-01-15 20:29:39 +0100

    feat: emit PowerUpBarEvent for power-up bar creation and destruction

--- server/src/ecs/systems/powerUpBar/PowerUpBarSystem.cpp
+++ server/src/ecs/systems/powerUpBar/PowerUpBarSystem.cpp
@@ -15,21 +15,13 @@ namespace Game
 
         int playerIndex = 0;
         reg.view<Ecs::PlayerPowerUp, Ecs::Position>(
-            [&](const Ecs::Entity, Ecs::PlayerPowerUp &powerUp, const Ecs::Position &) {
+            [&](const Ecs::Entity playerEntity, Ecs::PlayerPowerUp &powerUp, const Ecs::Position &) {
                 if (powerUp.hasPowerUp && !powerUp.hasBar) {
-                    const float yOffset = (static_cast<float>(playerIndex) * 40.f);
-                    const Ecs::Entity barEntity = world.createEntity();
-                    reg.emplaceComponent<Ecs::Position>(barEntity, Ecs::Position{10.f, yOffset});
-                    reg.emplaceComponent<Ecs::Velocity>(barEntity, Ecs::Velocity{0.f, 0.f});
-                    reg.emplaceComponent<Ecs::Drawable>(barEntity, Ecs::Drawable{16, true});
-                    reg.emplaceComponent<Ecs::Id>(
-                        barEntity, Ecs::Id{static_cast<uint32_t>(static_cast<size_t>(barEntity))});
-                    powerUp.hasBar = true;
-                    powerUp.barEntity = barEntity;
+                    world.events().emit<PowerUpBarEvent>(
+                        PowerUpBarEvent{static_cast<size_t>(playerEntity), true, playerIndex});
                 } else if (!powerUp.hasPowerUp && powerUp.hasBar && powerUp.barEntity.has_value()) {
-                    world.destroyEntity(powerUp.barEntity.value());
-                    powerUp.hasBar = false;
-                    powerUp.barEntity = std::nullopt;
+                    world.events().emit<PowerUpBarEvent>(
+                        PowerUpBarEvent{static_cast<size_t>(playerEntity), false, playerIndex});
                 }
                 playerIndex++;
             });

4e31cca96189e7428e599c445b1e6875e99697d3
ann7415 <anna.poghosyan@epitech.eu>
2026-01-16 01:27:11 +0100

    fix: remove redundant parameter from SettingsState constructor for clarity

--- client/src/engine/state/settings/SettingsState.cpp
+++ client/src/engine/state/settings/SettingsState.cpp
@@ -12,7 +12,8 @@ namespace Engine
     SettingsState::SettingsState(std::shared_ptr<Graphics::IGraphics> graphics,
         std::shared_ptr<Graphics::IRenderer> renderer, std::shared_ptr<MusicRegistry> musicRegistry,
         std::shared_ptr<SoundRegistry> soundRegistry, std::shared_ptr<RoomManager> roomManager,
-        std::shared_ptr<EventBus> eventBus, std::shared_ptr<AuthContext> authCtx, std::shared_ptr<ScoreboardContext> scoreCtx)
+        std::shared_ptr<EventBus> eventBus, std::shared_ptr<AuthContext> authCtx,
+        std::shared_ptr<ScoreboardContext> scoreCtx)
         : _graphics(std::move(graphics)), _renderer(std::move(renderer)), _musicRegistry(std::move(musicRegistry)),
           _soundRegistry(std::move(soundRegistry)), _roomManager(std::move(roomManager)),
           _eventBus(std::move(eventBus)), _authCtx(std::move(authCtx)), _scoreCtx(std::move(scoreCtx))

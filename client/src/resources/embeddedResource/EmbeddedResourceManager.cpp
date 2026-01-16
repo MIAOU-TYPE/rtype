72f624ad469a9c77a1810ad93a901e710a5c77d8
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-16 09:16:09 +0100

    feat: add multi-phase boss resources and update asset loading for boss3

--- client/src/resources/embeddedResource/EmbeddedResourceManager.cpp
+++ client/src/resources/embeddedResource/EmbeddedResourceManager.cpp
@@ -61,8 +61,20 @@ namespace EmbeddedResources
     extern const unsigned int boss_png_size;
     extern const unsigned char boss2_png[];
     extern const unsigned int boss2_png_size;
-    extern const unsigned char boss3_png[];
-    extern const unsigned int boss3_png_size;
+    extern const unsigned char boss3P1_png[];
+    extern const unsigned int boss3P1_png_size;
+    extern const unsigned char boss3P2_png[];
+    extern const unsigned int boss3P2_png_size;
+    extern const unsigned char boss3P3_png[];
+    extern const unsigned int boss3P3_png_size;
+    extern const unsigned char boss3P4_png[];
+    extern const unsigned int boss3P4_png_size;
+    extern const unsigned char boss3P5_png[];
+    extern const unsigned int boss3P5_png_size;
+    extern const unsigned char boss3P6_png[];
+    extern const unsigned int boss3P6_png_size;
+    extern const unsigned char boss3P7_png[];
+    extern const unsigned int boss3P7_png_size;
     extern const unsigned char obstacle_png[];
     extern const unsigned int obstacle_png_size;
 
@@ -144,7 +156,13 @@ void EmbeddedResourceManager::registerResources()
     _resources["sprites/explose.png"] = {EmbeddedResources::explose_png, EmbeddedResources::explose_png_size};
     _resources["sprites/boss.png"] = {EmbeddedResources::boss_png, EmbeddedResources::boss_png_size};
     _resources["sprites/boss2.png"] = {EmbeddedResources::boss2_png, EmbeddedResources::boss2_png_size};
-    _resources["sprites/boss3.png"] = {EmbeddedResources::boss3_png, EmbeddedResources::boss3_png_size};
+    _resources["sprites/boss3/boss3P1.png"] = {EmbeddedResources::boss3P1_png, EmbeddedResources::boss3P1_png_size};
+    _resources["sprites/boss3/boss3P2.png"] = {EmbeddedResources::boss3P2_png, EmbeddedResources::boss3P2_png_size};
+    _resources["sprites/boss3/boss3P3.png"] = {EmbeddedResources::boss3P3_png, EmbeddedResources::boss3P3_png_size};
+    _resources["sprites/boss3/boss3P4.png"] = {EmbeddedResources::boss3P4_png, EmbeddedResources::boss3P4_png_size};
+    _resources["sprites/boss3/boss3P5.png"] = {EmbeddedResources::boss3P5_png, EmbeddedResources::boss3P5_png_size};
+    _resources["sprites/boss3/boss3P6.png"] = {EmbeddedResources::boss3P6_png, EmbeddedResources::boss3P6_png_size};
+    _resources["sprites/boss3/boss3P7.png"] = {EmbeddedResources::boss3P7_png, EmbeddedResources::boss3P7_png_size};
     _resources["sprites/obstacle.png"] = {EmbeddedResources::obstacle_png, EmbeddedResources::obstacle_png_size};
 
     _resources["shaders/colorblind.frag"] = {

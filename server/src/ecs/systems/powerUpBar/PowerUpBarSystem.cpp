77ba07791ddc42a7bf4af6a273e628b3e0edd31a
EvannBloutin <evann.bloutin@epitech.eu>
2026-01-17 00:11:26 +0100

    refactor: improve code formatting and readability in PowerUpBarSystem

--- server/src/ecs/systems/powerUpBar/PowerUpBarSystem.cpp
+++ server/src/ecs/systems/powerUpBar/PowerUpBarSystem.cpp
@@ -13,14 +13,14 @@ namespace Game
     {
         auto &reg = world.registry();
 
-        std::vector<std::pair<Ecs::Entity, Ecs::PlayerPowerUp*>> playersWithPowerUp;
-        reg.view<Ecs::PlayerPowerUp>(
-            [&](const Ecs::Entity playerEntity, Ecs::PlayerPowerUp &powerUp) {
-                playersWithPowerUp.emplace_back(playerEntity, &powerUp);
-            });
+        std::vector<std::pair<Ecs::Entity, Ecs::PlayerPowerUp *>> playersWithPowerUp;
+        reg.view<Ecs::PlayerPowerUp>([&](const Ecs::Entity playerEntity, Ecs::PlayerPowerUp &powerUp) {
+            playersWithPowerUp.emplace_back(playerEntity, &powerUp);
+        });
 
-        std::sort(playersWithPowerUp.begin(), playersWithPowerUp.end(),
-            [](const auto &a, const auto &b) { return static_cast<size_t>(a.first) < static_cast<size_t>(b.first); });
+        std::sort(playersWithPowerUp.begin(), playersWithPowerUp.end(), [](const auto &a, const auto &b) {
+            return static_cast<size_t>(a.first) < static_cast<size_t>(b.first);
+        });
 
         int playerIndex = 0;
         for (auto &[playerEntity, powerUp] : playersWithPowerUp) {

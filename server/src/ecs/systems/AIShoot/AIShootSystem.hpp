240bc5544f220d7c8a6475368054b068e7a335a9
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-16 16:41:34 +0100

    feat: add spreadProjectile method to AIShootSystem for handling spread shooting behavior

--- server/src/ecs/systems/AIShoot/AIShootSystem.hpp
+++ server/src/ecs/systems/AIShoot/AIShootSystem.hpp
@@ -32,5 +32,17 @@ namespace Game
          * @param dt The delta time since the last update.
          */
         static void update(IGameWorld &world, float dt);
+
+        /**
+         * @brief Handle spread projectile shooting pattern.
+         * @param world The game world containing entities.
+         * @param ent The entity that is shooting.
+         * @param shoot The AIShoot component of the entity.
+         * @param posX The X position of the muzzle.
+         * @param posY The Y position of the muzzle.
+         * @param weapon The WeaponConfig component of the entity.
+         */
+        static void spreadProjectile(IGameWorld &world, const Ecs::Entity ent, const Ecs::AIShoot &shoot,
+          const float posX, const float posY, const Ecs::WeaponConfig &weapon);
     };
 } // namespace Game

1595c9ac1ac46f5c87f70adb8a49296b9d65d913
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-17 00:32:06 +0100

    feat: add BossPart component for boss hitbox zones and damage multipliers

---
+++ server/src/ecs/components/BossPart.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** rtype
+** File description:
+** BossPart
+*/
+
+#pragma once
+
+#include "Entity.hpp"
+
+namespace Ecs {
+    /**
+     * @brief Component representing hitbox zones for a boss entity
+     * This component defines multiple hitbox zones for a boss,
+     * each with its own damage multiplier. This allows for bosses
+     * to have weak points or areas that take reduced damage.
+     * */
+    struct BossPart {
+        Ecs::Entity bossEntity; ///> The main boss entity this part belongs to
+        float damageMultiplier; ///> Damage multiplier for this part
+
+    };
+} // namespace Ecs

f95fd8e16ced148e6f002ae74d3eb30b002c7278
EvannBloutin <evann.bloutin@epitech.eu>
2026-01-17 00:12:03 +0100

    feat: destroy power-ups when player dies to improve game mechanics

--- server/src/game/world/World.cpp
+++ server/src/game/world/World.cpp
@@ -77,13 +77,16 @@ namespace
 
             if (const auto &playerPowerUp = w->registry().getComponents<Ecs::PlayerPowerUp>().at(event.target)) {
                 if (playerPowerUp->powerUpEntity.has_value())
-                    w->events().emit<DestroyEvent>(DestroyEvent{static_cast<size_t>(playerPowerUp->powerUpEntity.value()), false});
+                    w->events().emit<DestroyEvent>(
+                        DestroyEvent{static_cast<size_t>(playerPowerUp->powerUpEntity.value()), false});
                 if (playerPowerUp->hasBar && playerPowerUp->barEntity.has_value())
-                    w->events().emit<DestroyEvent>(DestroyEvent{static_cast<size_t>(playerPowerUp->barEntity.value()), false});
+                    w->events().emit<DestroyEvent>(
+                        DestroyEvent{static_cast<size_t>(playerPowerUp->barEntity.value()), false});
             }
             if (const auto &bubblePowerUp = w->registry().getComponents<Ecs::BubblePowerUp>().at(event.target)) {
                 if (bubblePowerUp->bubbleEntity.has_value())
-                    w->events().emit<DestroyEvent>(DestroyEvent{static_cast<size_t>(bubblePowerUp->bubbleEntity.value()), false});
+                    w->events().emit<DestroyEvent>(
+                        DestroyEvent{static_cast<size_t>(bubblePowerUp->bubbleEntity.value()), false});
             }
         });
     }
@@ -246,6 +249,8 @@ namespace
         if (playerPowerUp)
             return;
 
+        reg.getComponents<Ecs::PowerUp>().remove(powerUpIdx);
+
         if (auto &drawable = reg.getComponents<Ecs::Drawable>().at(powerUpIdx))
             drawable->spriteId = 14;
 
@@ -293,6 +298,12 @@ namespace
             }
 
             const auto &powerUpType = reg.getComponents<Ecs::PowerUpType>().at(powerUpIdx);
+            if (powerUpType && powerUpType->type == Ecs::PowerUpTypeEnum::Standard) {
+                const auto &playerPowerUp = reg.getComponents<Ecs::PlayerPowerUp>().at(playerIdx);
+                if (playerPowerUp) {
+                    return;
+                }
+            }
 
             if (powerUpType && powerUpType->type == Ecs::PowerUpTypeEnum::Laser) {
                 activateLaserPowerUp(w, powerUpIdx, playerIdx);

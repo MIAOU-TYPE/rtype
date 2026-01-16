f5c975cd8a3c5c396203bf654c4be85d84e973fe
ann7415 <anna.poghosyan@epitech.eu>
2026-01-16 01:26:11 +0100

    feat: refactor ScoreboardGetRequested constructor for clarity

--- client/src/events/Event.hpp
+++ client/src/events/Event.hpp
@@ -204,8 +204,10 @@ namespace Engine
          * @brief Constructor for ScoreboardGetRequested event.
          * @param limit_ The maximum number of scores to retrieve.
          */
-        explicit ScoreboardGetRequested(const std::size_t limit_ = 10) : limit(limit_) {}
+        explicit ScoreboardGetRequested(const std::size_t limit_ = 10) : limit(limit_)
+        {
+        }
 
-        std::size_t limit;  ///> The maximum number of scores to retrieve.
+        std::size_t limit; ///> The maximum number of scores to retrieve.
     };
 } // namespace Engine

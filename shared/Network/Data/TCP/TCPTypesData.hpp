fcac0eda2e4dfa5b156e221d50061bd5bdf95aba
romain1717 <romain.berard@epitech.eu>
2026-01-17 04:51:38 +0100

    feat: add ROOM_INFO packet type for client to request current lobby information

--- shared/Network/Data/TCP/TCPTypesData.hpp
+++ shared/Network/Data/TCP/TCPTypesData.hpp
@@ -32,6 +32,7 @@ namespace Net::Protocol::TCP
     constexpr uint8_t AUTH_REGISTER = 0x08;  ///> Client requests to register a new account
     constexpr uint8_t AUTH_LOGIN = 0x09;     ///> Client requests to log in to an existing account
     constexpr uint8_t SCOREBOARD_GET = 0x0A; ///> Client requests global scoreboard (top N)
+    constexpr uint8_t ROOM_INFO = 0x0B;
 
     /**
      * @brief Packet types for TCP communication from server to client.

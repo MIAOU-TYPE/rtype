13e81715c2be97e8df190202f0a484e6e3f8969e
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2026-01-18 17:01:41 +0100

    feat: add PixelCollision component for pixel-perfect collision detection

---
+++ server/src/ecs/components/PixelCollision.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2026
+** rtype
+** File description:
+** PixelCollision
+*/
+
+#pragma once
+
+namespace Ecs
+{
+    /**
+     * @struct PixelCollision
+     * @brief Component for pixel-perfect collision detection.
+     */
+    struct PixelCollision {
+        bool useAlpha = false;      ///> Whether to use alpha channel for collision detection
+        float tileWidth = 0.f; ///> Width of the collision tile
+        float tileHeight = 0.f; ///> Height of the collision tile
+    };
+} // namespace Ecs

bee6b5e0bbc90a57fd00da4da622d514ce09bef9
romain1717 <romain.berard@epitech.eu>
2026-01-18 01:16:39 +0100

    feat: update .gitignore to include coverage directory and add coverage script

---
+++ scripts/coverage.sh
@@ -0,0 +1,12 @@
+rm -fr coverage
+mkdir -p coverage
+cmake --build build -j
+ctest --test-dir build
+gcovr -r . --object-directory build \
+  --exclude '.*vcpkg_installed/.*' \
+  --exclude '.*server/tests/.*' \
+  --exclude '.*tests/.*' \
+  --exclude '.*.hpp' \
+  --html --html-details -o coverage/index.html
+
+open coverage/index.html
